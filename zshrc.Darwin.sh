769ebbbdae750d3307608aaa78b7f1bd9712fc2c
Robert Laverty <roblav96@gmail.com>
2019-03-21 00:21:24 -0400

    tree

--- zshrc.Darwin.sh
+++ zshrc.Darwin.sh
@@ -12,9 +12,10 @@ export MANPAGER=$PAGER
 export NIX_PAGER=$PAGER
 export BAT_PAGER=$PAGER
 
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 zstyle :omz:plugins:ssh-agent agent-forwarding on
 zstyle :omz:plugins:ssh-agent identities id_rsa tunnel_rsa
