12b5f6f8732aedd9b63fafd33039da30d36ed4db
Robert Laverty <roblav96@gmail.com>
2019-03-20 22:07:55 -0400

    instal bash

---
+++ install.bash.sh
@@ -0,0 +1,63 @@
+
+DOTFILES="$(cd "$(dirname "$0")"; pwd -P)"
+[ -z "$DEV" ] && (cd $DOTFILES && git pull --quiet && git reset --quiet --hard)
+BASHRC="$(cd "$(dirname "$0")/.."; pwd -P)/.bashrc"
+
+echo > $BASHRC
+echo "export DOTFILES='$DOTFILES'" >> $BASHRC
+for i in "zshrc" "zshrc.$(uname -s)"; do
+	echo "[ -f '$DOTFILES/static/.profile' ] && source '$DOTFILES/static/.profile'" >> $BASHRC
+done
+echo "alias src='sh $DOTFILES/install.bash.sh'" >> $BASHRC
+# cat $ZSHRC
+
+clear
+reset
+# exec $(echo "$0")
+
+echo; echo '⬤ dotfiles install.sh complete'; echo
+
+
+
+# export DOTFILES=""
+# echo [ -f "$DOTFILES/zshrc.sh" ] && source "$DOTFILES/zshrc.sh" >> $ZSHRC
+# echo `echo '[ -f "$DOTFILES/zshrc.$(uname -s).sh" ] && source "$DOTFILES/zshrc.$(uname -s).sh"'` >> $ZSHRC
+
+# mkdir -p "$DOTFILES/.bin"
+
+
+
+# echo
+
+# DIR="${0%/*}"
+# echo "DIR -> $DIR"
+# ZSHRC="$DIR/../.zshrc"
+# echo "ZSHRC -> $ZSHRC"
+# cp "$DIR/static/.zshrc" $ZSHRC
+# cat $ZSHRC
+# mkdir -p "$DIR/.bin"
+
+# echo
+
+# DIR="${0%/*}"
+# echo "DIR -> $DIR"
+# SCRIPT="`readlink -f "$0"`"
+# echo "SCRIPT -> $SCRIPT"
+# ZSHRC="$DIR/../.zshrc"
+# echo "ZSHRC -> $ZSHRC"
+# DOTFILES="`cd "$(dirname "$0")"; pwd -P`"
+# echo "DOTFILES -> $DOTFILES"
+
+# DOTFILES=".dotfiles"
+# ZSHRC="$DOTFILES/../.zshrc"
+# echo "ZSHRC -> $ZSHRC"
+# cat $ZSHRC
+# # echo > $ZSHRC
+# # echo '[ -f "$DOTFILES/zshrc.sh" ] && source "$DOTFILES/zshrc.sh"' >> $ZSHRC
+# # echo '[ -f "$DOTFILES/zshrc.$(uname -s).sh" ] && source "$DOTFILES/zshrc.$(uname -s).sh"' >> $ZSHRC
+
+# DIR="${0%/*}"
+# DIR="$(readlink -f "$0")"
+# DIR=$(readlink -f "$0")
+# echo "DIR -> $DIR"
+# export DOTFILES="$DIR/.dotfiles"
