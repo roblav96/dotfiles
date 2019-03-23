7c5e1ba2c24a6c888abd312d188a7acbe005d7ba
Robert Laverty <roblav96@gmail.com>
2019-03-23 18:34:20 -0400

    linux

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -1,12 +1,4 @@
 
-local NAME="server"
-export PURE_PROMPT_SYMBOL="%f%F{red}$NAME%f %F{magenta}▶"
-
-export FZF_BASE="/usr/local/bin"
-
-export CLICOLOR="true"
-export LSCOLORS="ExFxBxDxCxegedabagacad"
-
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH="$GOBIN:$PATH"
---
+++ zshrc.Linux.before.sh
@@ -0,0 +1,8 @@
+
+local NAME="server"
+export PURE_PROMPT_SYMBOL="%f%F{red}$NAME%f %F{magenta}▶"
+
+export FZF_BASE="/usr/local/bin"
+
+export CLICOLOR="true"
+export LSCOLORS="ExFxBxDxCxegedabagacad"
