1dfbf4cb99525b0e39b4262d7ae8632a23068575
Robert Laverty <roblav96@gmail.com>
2019-03-23 17:54:47 -0400

    cli color

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -1,6 +1,9 @@
 
 local NAME="server"
-export PURE_PROMPT_SYMBOL="%f%F{red}$NAME%f %F{magenta}➜"
+export PURE_PROMPT_SYMBOL="%f%F{red}$NAME%f %F{magenta}▶"
+
+export CLICOLOR="true"
+export LSCOLORS="ExFxBxDxCxegedabagacad"
 
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
