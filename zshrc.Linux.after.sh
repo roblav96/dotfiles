2c37f7dde099993304439b911b7de31111e8eb13
Robert Laverty <roblav96@gmail.com>
2019-03-23 18:32:42 -0400

    dirnam

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -2,7 +2,7 @@
 local NAME="server"
 export PURE_PROMPT_SYMBOL="%f%F{red}$NAME%f %F{magenta}▶"
 
-export FZF_BASE="dirname `which fzf`"
+export FZF_BASE="/usr/local/bin"
 
 export CLICOLOR="true"
 export LSCOLORS="ExFxBxDxCxegedabagacad"
