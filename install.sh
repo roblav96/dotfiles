d71c6f08799ca6bb130e850811d0c7bd64085015
Robert Laverty <roblav96@gmail.com>
2019-03-23 17:24:26 -0400

    moved

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ ZSHRC="$(cd "$(dirname "$0")/.."; pwd -P)/.zshrc"
 
 echo > $ZSHRC
 echo "export DOTFILES='$DOTFILES'" >> $ZSHRC
+echo "alias src='sh $DOTFILES/install.sh'" >> $ZSHRC
 echo "[ -f '$DOTFILES/zshrc.$(uname -s).before.sh' ] && source '$DOTFILES/zshrc.$(uname -s).before.sh'" >> $ZSHRC
 echo "[ -f '$DOTFILES/zshrc.sh' ] && source '$DOTFILES/zshrc.sh'" >> $ZSHRC
 echo "[ -f '$DOTFILES/zshrc.$(uname -s).after.sh' ] && source '$DOTFILES/zshrc.$(uname -s).after.sh'" >> $ZSHRC
-echo "alias src='sh $DOTFILES/install.sh'" >> $ZSHRC
 cat $ZSHRC
 
 # clear
