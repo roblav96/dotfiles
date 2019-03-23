9ddb79b6604d3f94b87d4bc446e455e8bf1c7488
Robert Laverty <roblav96@gmail.com>
2019-03-23 18:40:45 -0400

    fzf

--- zshrc.Linux.before.sh
+++ zshrc.Linux.before.sh
@@ -2,7 +2,8 @@
 local NAME="server"
 export PURE_PROMPT_SYMBOL="%f%F{red}$NAME%f %F{magenta}▶"
 
-export FZF_BASE="/usr/local/bin"
-
 export CLICOLOR="true"
 export LSCOLORS="ExFxBxDxCxegedabagacad"
+
+export FZF_BASE="$HOME/.fzf"
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
