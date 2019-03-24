a1c30d3f5aa736dfe995a6343d5dd2fe78aa06e4
Robert Laverty <roblav96@gmail.com>
2019-03-24 18:56:54 -0400

    Sun Mar 24 18:56:54 EDT 2019

--- zshrc.Darwin.after.sh
+++ zshrc.Darwin.after.sh
@@ -5,11 +5,18 @@ unalias src
 source "$DOTFILES/bashrc.sh"
 
 alias dot="cd $DOTFILES && st $DOTFILES"
+function dotpush() {
+	git add -A
+	git commit -a -m "`date`"
+	git push origin master
+}
 # alias dotpush="git add -A && git commit -a -m "`date`" && git push origin master"
 # alias dotpush="git commit -a -m `date` && git push origin master"
 # alias dotpush="git commit -a --amend --no-edit && git push --force origin master"
 
-function adbsu() { adb shell su -c ${@:2} }
+function adbsu() {
+	adb shell su -c ${@:2}
+}
 
 source "$DOTFILES/modules/brew.sh"
 source "$DOTFILES/modules/deving.sh"
