e91ea4c9266c71c8c9ad70aa189b538d6a735d7f
Robert Laverty <roblav96@gmail.com>
2019-03-30 09:37:00 -0400

    M zshrc.Darwin.before.sh

--- zshrc.Darwin.before.sh
+++ zshrc.Darwin.before.sh
@@ -2,6 +2,7 @@
 export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
