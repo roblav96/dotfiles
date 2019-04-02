bfa29778e7b01c4910c2283e5d387cb174f17e0b
Robert Laverty <roblav96@gmail.com>
2019-04-02 03:47:10 -0400

    M ssl-certs/futon.media.sh

--- ssl-certs/futon.media.sh
+++ ssl-certs/futon.media.sh
@@ -1,2 +1,8 @@
 # lego -s https://acme-staging-v02.api.letsencrypt.org/directory
 lego -d futon.media -a -m roblav96@gmx.com --http.webroot /var/www --path /root/.lego --http renew
+
+openssl pkcs12 -export \
+	-inkey /root/.lego/certificates/futon.media.key \
+	-in /root/.lego/certificates/futon.media.crt \
+	-certfile /root/.lego/certificates/futon.media.issuer.crt \
+	-out /opt/emby-server/etc/ssl/certs/futon.media.pfx
