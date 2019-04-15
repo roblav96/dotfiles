9fcd56604cfc928a57a0113a3ebcf64f4d4f269b
Robert Laverty <roblav96@gmail.com>
2019-04-15 19:57:46 -0400

    M ssl-certs/futon.media.sh

--- ssl-certs/futon.media.sh
+++ ssl-certs/futon.media.sh
@@ -2,3 +2,4 @@
 lego -d futon.media -a -m roblav96@gmx.com --http.webroot /var/www --path /root/.lego --http renew
 
 openssl pkcs12 -export -inkey /root/.lego/certificates/futon.media.key -in /root/.lego/certificates/futon.media.crt -certfile /root/.lego/certificates/futon.media.issuer.crt -out /opt/emby-server/etc/ssl/certs/futon.media.pfx -passout pass:
+chmod 644 /opt/emby-server/etc/ssl/certs/futon.media.pfx
