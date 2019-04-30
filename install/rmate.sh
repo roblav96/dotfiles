19ef6c9d39af0d2a2e7e7e8d39e985bf69736f29
Robert Laverty <roblav96@gmail.com>
2019-04-30 09:33:25 -0400

    [Darwin]  M install/rmate.sh

--- install/rmate.sh
+++ install/rmate.sh
@@ -1,4 +1,4 @@
 
 mkdir -p "$HOME/.bin"
-wget -O "$HOME/.bin/st" "https://raw.githubusercontent.com/aurora/rmate/master/rmate"
+curl "https://raw.githubusercontent.com/aurora/rmate/master/rmate" -o "$HOME/.bin/st"
 chmod a+x "$HOME/.bin/st"
