d96057f0e700d5a2518a943c4d8411da904383d5
Robert Laverty <roblav96@gmail.com>
2019-05-11 04:42:17 -0400

    [Darwin]  M zshrc.Linux.after.sh

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -4,6 +4,6 @@ export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH="$GOBIN:$PATH"
 
-if [[ -x "$(which pm2)" ]]; then
-	source <(pm2 completion)
-fi
+# if [[ -x "$(which pm2)" ]]; then
+# 	source <(pm2 completion)
+# fi
