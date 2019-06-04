bb4fd4ee80726a75d0a10dcf34ac6f66c140c4aa
Robert Laverty <roblav96@gmail.com>
2019-06-04 07:15:03 -0400

    [Darwin]  M zshrc.Android.before.sh

--- zshrc.Android.before.sh
+++ zshrc.Android.before.sh
@@ -4,7 +4,7 @@ export TERM="xterm-256color"
 export CLICOLOR="1"
 export LSCOLORS="ExFxBxDxCxegedabagacad"
 
-export FZF_BASE="/data/data/com.termux/files/usr/share/fzf"
+export FZF_BASE="/data/data/com.termux/files/usr/share"
 # export FZF_BASE="$HOME/.fzf"
 
 test -x "`which whoami`" && export USER="`whoami`"
