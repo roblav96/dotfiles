cab709dfaec7c4d1ed892908ab1cce30a78b897f
Robert Laverty <roblav96@gmail.com>
2019-06-18 07:14:05 -0400

    [Darwin]  M ssl-certs/emby.futon.media.sh

--- ssl-certs/emby.futon.media.sh
+++ ssl-certs/emby.futon.media.sh
@@ -1,5 +1,5 @@
 # lego -s https://acme-staging-v02.api.letsencrypt.org/directory
 lego -d emby.futon.media -a -m roblav96@gmx.com --http.webroot /var/www --path /root/.lego --http renew
 
-openssl pkcs12 -export -inkey /root/.lego/certificates/emby.futon.media.key -in /root/.lego/certificates/emby.futon.media.crt -certfile /root/.lego/certificates/emby.futon.media.issuer.crt -out /opt/emby-server/etc/ssl/certs/emby.futon.media.pfx -passout pass:
-chmod 644 /opt/emby-server/etc/ssl/certs/emby.futon.media.pfx
+# openssl pkcs12 -export -inkey /root/.lego/certificates/emby.futon.media.key -in /root/.lego/certificates/emby.futon.media.crt -certfile /root/.lego/certificates/emby.futon.media.issuer.crt -out /opt/emby-server/etc/ssl/certs/emby.futon.media.pfx -passout pass:
+# chmod 644 /opt/emby-server/etc/ssl/certs/emby.futon.media.pfx
