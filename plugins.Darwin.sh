521f6fb51fad014427e15b6f06f9da09fff3c42e
Robert Laverty <roblav96@gmail.com>
2019-07-05 13:13:05 -0400

    [Darwin]  M plugins.Darwin.sh

--- plugins.Darwin.sh
+++ plugins.Darwin.sh
@@ -1,9 +1,11 @@
 # rgcr/m-cli path:completion/zsh kind:fpath
 robbyrussell/oh-my-zsh path:plugins/adb
+robbyrussell/oh-my-zsh path:plugins/bundler
 robbyrussell/oh-my-zsh path:plugins/gem
 robbyrussell/oh-my-zsh path:plugins/gradle
 robbyrussell/oh-my-zsh path:plugins/osx
 robbyrussell/oh-my-zsh path:plugins/pod
+# robbyrussell/oh-my-zsh path:plugins/rails
 robbyrussell/oh-my-zsh path:plugins/ruby
 robbyrussell/oh-my-zsh path:plugins/ssh-agent
 robbyrussell/oh-my-zsh path:plugins/sublime
