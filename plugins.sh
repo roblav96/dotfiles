d41d08184ddaf864e7e2daef427838d288cf89ce
Robert Laverty <roblav96@gmail.com>
2019-07-25 02:07:43 -0400

    damn annoying char encodings...

--- plugins.sh
+++ plugins.sh
@@ -3,12 +3,12 @@ caarlos0/zsh-mkc
 changyuheng/fz
 mafredri/zsh-async
 trapd00r/LS_COLORS kind:fpath
-zsh-users/zaw
+# zsh-users/zaw
 zsh-users/zsh-autosuggestions
 zsh-users/zsh-completions
 # ████  KEEP AT BOTTOM  ████
 sindresorhus/pure
 # geometry-zsh/geometry
-# zdharma/fast-syntax-highlighting
-zsh-users/zsh-syntax-highlighting
+# zsh-users/zsh-syntax-highlighting
+zdharma/fast-syntax-highlighting
 zsh-users/zsh-history-substring-search
