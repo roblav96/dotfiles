091eff8ea2b14a86850243e7966d5bf4a269e0e0
Robert Laverty <roblav96@gmail.com>
2019-08-14 18:12:23 -0400

    [Darwin]  M modules/ffsend.zsh

--- modules/ffsend.zsh
+++ modules/ffsend.zsh
@@ -1,4 +1,5 @@
 
-export FFSEND_HISTORY="/dev/null"
-export FFSEND_INCOGNITO="1"
-export FFSEND_VERBOSE="1"
+export FFSEND_HISTORY='/dev/null'
+export FFSEND_INCOGNITO='1'
+export FFSEND_VERBOSE='1'
+export FFSEND_API='3'
