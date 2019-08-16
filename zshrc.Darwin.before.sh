640e91f1e3ae0b7370410de3d7dd20ed755c7919
Robert Laverty <roblav96@gmail.com>
2019-08-16 00:37:31 -0400

    [Darwin]  M zshrc.Darwin.before.sh

--- zshrc.Darwin.before.sh
+++ zshrc.Darwin.before.sh
@@ -1,12 +1,3 @@
-export PATH="/usr/local/sbin:$PATH"
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
-export PATH="/usr/local/opt/man-db/libexec/bin:$PATH"
-# export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-
 zstyle :omz:plugins:ssh-agent agent-forwarding on
 zstyle :omz:plugins:ssh-agent identities id_rsa
 # zstyle :omz:plugins:ssh-agent identities id_rsa tunnel_rsa
