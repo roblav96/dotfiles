9d0a6019b4c30e590b2154ca8caf8ebddb3d0d68
Robert Laverty <roblav96@gmail.com>
2019-08-28 05:32:47 -0400

    [Darwin]  M zshrc.Android.before.sh

--- zshrc.Android.before.sh
+++ zshrc.Android.before.sh
@@ -4,7 +4,7 @@ export SHELL="$(which zsh)"
 export CLICOLOR="1"
 export LSCOLORS="ExFxBxDxCxegedabagacad"
 
-export FZF_BASE="$HOME/.fzf"
+export FZF_BASE="/data/data/com.termux/files/usr/share/fzf"
 
 test -x "$(which whoami)" && export USER="$(whoami)"
 if [[ "$USER" == "root" ]]; then
