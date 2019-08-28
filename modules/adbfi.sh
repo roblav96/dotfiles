d4edc6e55b72923a56fd677b35ba00e010d043d9
Robert Laverty <roblav96@gmail.com>
2019-08-28 10:12:49 -0400

    [Darwin]  M modules/adbfi.sh

--- modules/adbfi.sh
+++ modules/adbfi.sh
@@ -1,20 +1,19 @@
+#!/usr/bin/env sh
 
-adbfi() {
-	case $1 in
-		on) setprop service.adb.tcp.port 5555
-			su -c stop adbd
-			su -c start adbd
-			echo "ADB over WiFi enabled";;
-		off) setprop service.adb.tcp.port -1
-			su -c stop adbd
-			su -c start adbd
-			echo "ADB over WiFi disabled";;
-		status) case `getprop service.adb.tcp.port` in -1) echo "off";; 5555) echo "on";; *) echo "off";; esac;;
-		*) echo "Usage: adbfi [option]"
-			echo " "
-			echo "Options:"
-			echo " on    - Enables ADB over Wifi"
-			echo " off   - Disables ADB over WiFi"
-			echo " status - Gets current status";;
-	esac
-}
+case $1 in
+	on) setprop service.adb.tcp.port 5555
+		su -c stop adbd
+		su -c start adbd
+		echo "ADB over WiFi enabled";;
+	off) setprop service.adb.tcp.port -1
+		su -c stop adbd
+		su -c start adbd
+		echo "ADB over WiFi disabled";;
+	status) case `getprop service.adb.tcp.port` in -1) echo "off";; 5555) echo "on";; *) echo "off";; esac;;
+	*) echo "Usage: adbfi [option]"
+		echo " "
+		echo "Options:"
+		echo " on    - Enables ADB over Wifi"
+		echo " off   - Disables ADB over WiFi"
+		echo " status - Gets current status";;
+esac
