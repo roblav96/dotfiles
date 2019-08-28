d481cf97d66e2c38dae29a6f55593fe86384e623
Robert Laverty <roblav96@gmail.com>
2019-08-28 10:30:16 -0400

    [Darwin]  M zshrc.Android.after.sh

--- zshrc.Android.after.sh
+++ zshrc.Android.after.sh
@@ -22,5 +22,3 @@ test -d "/system/xbin" && export PATH="$PATH:/system/xbin"
 test -d "/system/bin" && export PATH="$PATH:/system/bin"
 test -d "/vendor/bin" && export PATH="$PATH:/vendor/bin"
 test -d "/sbin" && export PATH="$PATH:/sbin"
-
-source "$DOTFILES/modules/adbfi.sh"
