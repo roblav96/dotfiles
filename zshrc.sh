30e65a0e9c3d14db3e1d56f6803ab5af6d1ccc8b
Robert Laverty <roblav96@gmail.com>
2019-08-30 00:33:02 -0400

    [Darwin]  M zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -116,25 +116,27 @@ alias gs='git status'
 alias gst='git standup'
 alias sedbat='sed -u -r "s/\"|\x27|\`//g" | bat -l rb'
 alias zbak='sudo cp ~/.zsh_history ~/..zsh_history; sudo cp ~/.z ~/..z'
+alias yarn='npx yarn'
+alias ember='npx ember'
 # alias sedbat='sd "\"|\x27|\`" "" | bat -l rb'
 # alias type="type -as"
 # alias ll="ls -lAFhnU"
 # alias man="man -P more"
 
 export EXA_COLORS="uu=2;37:gu=2;3;37:da=32:un=31:gn=2;3;31"
-alias l="exa --all --long --header --classify --group --modified --color-scale --icons --ignore-glob='.git|.DS_Store'"
-alias la="exa --all --long --header --classify --group --modified --color-scale --icons --extended --ignore-glob='.git|.DS_Store'"
-alias lr="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --level=2 --ignore-glob='.git|.DS_Store|node_modules'"
-alias lra="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --ignore-glob='.git|.DS_Store|node_modules'"
-alias lraa="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --ignore-glob='.git|.DS_Store' --extended"
-alias lm="exa --all --long --header --classify --group --modified --color-scale --icons --sort=modified --ignore-glob='.git|.DS_Store'"
-alias lmr="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --level=2 --ignore-glob='.git|.DS_Store|node_modules' --sort=modified"
-alias lmra="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --ignore-glob='.git|.DS_Store|node_modules' --sort=modified"
-alias lmraa="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --ignore-glob='.git|.DS_Store' --sort=modified --extended"
-alias lb="exa --all --long --header --classify --group --modified --color-scale --icons --sort=size --ignore-glob='.git|.DS_Store'"
-alias lbr="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --level=2 --ignore-glob='.git|.DS_Store|node_modules' --sort=size"
-alias lbra="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --ignore-glob='.git|.DS_Store|node_modules' --sort=size"
-alias lbraa="exa --all --long --header --classify --group --modified --color-scale --icons --recurse --tree --ignore-glob='.git|.DS_Store' --sort=size --extended"
+alias l="exa --all --long --header --classify --group --modified --color-scale --ignore-glob='.git|.DS_Store'"
+alias la="exa --all --long --header --classify --group --modified --color-scale --extended --ignore-glob='.git|.DS_Store'"
+alias lr="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --level=2 --ignore-glob='.git|.DS_Store|node_modules'"
+alias lra="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --ignore-glob='.git|.DS_Store|node_modules'"
+alias lraa="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --ignore-glob='.git|.DS_Store' --extended"
+alias lm="exa --all --long --header --classify --group --modified --color-scale --sort=modified --ignore-glob='.git|.DS_Store'"
+alias lmr="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --level=2 --ignore-glob='.git|.DS_Store|node_modules' --sort=modified"
+alias lmra="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --ignore-glob='.git|.DS_Store|node_modules' --sort=modified"
+alias lmraa="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --ignore-glob='.git|.DS_Store' --sort=modified --extended"
+alias lb="exa --all --long --header --classify --group --modified --color-scale --sort=size --ignore-glob='.git|.DS_Store'"
+alias lbr="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --level=2 --ignore-glob='.git|.DS_Store|node_modules' --sort=size"
+alias lbra="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --ignore-glob='.git|.DS_Store|node_modules' --sort=size"
+alias lbraa="exa --all --long --header --classify --group --modified --color-scale --recurse --tree --ignore-glob='.git|.DS_Store' --sort=size --extended"
 
 alias f="fd --color=always --hidden --no-ignore --fixed-strings --exclude='.git' --exclude='.DS_Store' --exclude='node_modules'"
 alias fa="fd --color=always --hidden --no-ignore --fixed-strings --exclude='.git' --exclude='.DS_Store'"
@@ -240,6 +242,14 @@ function batpl() {
 
 test -x "$(which tar)" && source "$DOTFILES/modules/tar.sh"
 test -x "$(which ffsend)" && source "$DOTFILES/modules/ffsend.zsh"
+
+if test -x "$(which ip)"; then
+	alias ip='ip -color -human -resolve'
+	alias ipm='ip -color -human -resolve -brief'
+	alias ipa='ip -color -human -resolve -all -details'
+	alias ipaa='ip -color -human -resolve -all -details -stats'
+	alias ipj='ip -color -human -resolve -all -details -stats -json'
+fi
 
 # autoload -U promptinit; promptinit
 # autoload -U compinit && compinit
