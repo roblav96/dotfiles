889ec28c2a9c94ce1a2841e0670c541dda85152c
Robert Laverty <roblav96@gmail.com>
2019-08-30 00:37:52 -0400

    [Darwin]  M zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -244,7 +244,7 @@ test -x "$(which tar)" && source "$DOTFILES/modules/tar.sh"
 test -x "$(which ffsend)" && source "$DOTFILES/modules/ffsend.zsh"
 
 if test -x "$(which ip)"; then
-	alias ipj='ip -human -resolve -all -details -stats -json -pretty'
+	alias ipj='ip -human -resolve -all -details -stats -json'
 	alias ip='ip -color -human -resolve'
 	alias ipm='ip -brief'
 	alias ipa='ip -all -details'
