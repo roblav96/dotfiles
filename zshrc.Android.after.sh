b44c5eb5abc84308553c0a2d691f25a04450e5fb
Robert Laverty <roblav96@gmail.com>
2019-08-30 02:21:00 -0400

    [Darwin]  M zshrc.Android.after.sh

--- zshrc.Android.after.sh
+++ zshrc.Android.after.sh
@@ -3,8 +3,8 @@ if [[ "$USER" == "root" ]]; then
 	export PURE_PROMPT_SYMBOL="⚠️  $USER ➤"
 	unset HISTFILE
 	unalias src
-	alias apt="echo !root"
-	alias pkg="echo !root"
+	alias apt="echo 'root will destroy apt file locks'"
+	alias pkg="echo 'root will destroy pkg file locks'"
 fi
 
 unalias sudo
