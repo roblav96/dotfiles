7d37fd06099b97e2dffa8dceeff466f60e796d50
Robert Laverty <roblav96@gmail.com>
2019-10-12 03:55:31 -0400

    [Darwin]  M modules/apt.sh

--- modules/apt.sh
+++ modules/apt.sh
@@ -2,4 +2,4 @@ alias aptsg="apt search 2>/dev/null | grep"
 alias aptlsg="apt list 2>/dev/null | grep"
 alias aptlsgi="apt list --installed 2>/dev/null | grep"
 
-function apts() { apt search $@ 2>/dev/null | grep "'$@'" }
+function apts() { apt search $@ 2>/dev/null | grep "$@" }
