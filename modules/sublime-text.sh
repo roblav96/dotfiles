ac229dbc676db11f5cf1ec301edd5c25771ea8aa
Robert Laverty <roblav96@gmail.com>
2019-10-12 00:20:58 -0400

    [Darwin]  M modules/sublime-text.sh

--- modules/sublime-text.sh
+++ modules/sublime-text.sh
@@ -1,4 +1,4 @@
-alias st="subl"
+alias st="subl -f"
 alias stp="subl --new-window --command 'project_manager {\"action\": \"switch\"}'"
 alias stcd="cd $HOME'/Library/Application Support/Sublime Text 3/Packages'"
 alias stst="subl $HOME'/Library/Application Support/Sublime Text 3/Packages'"
