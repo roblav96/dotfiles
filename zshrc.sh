d2658210fd2a5df54004ee99d7ebe60338157cf1
Robert Laverty <roblav96@gmail.com>
2019-10-12 05:57:25 -0400

    [Darwin]  M zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -142,6 +142,7 @@ alias gradlew='./gradlew'
 alias h="history | tail -n $(expr $(tput lines) - 10) | bat -l sh"
 alias aliasls="alias -L | sed 's/^/\n/g' | bat -l sh"
 alias dotcd="cd $DOTFILES"
+alias dotmodules="batt $DOTFILES/modules/* -l sh"
 # alias sedbat='sd "\"|\x27|\`" "" | bat -l rb'
 # alias type="type -as"
 # alias ll="ls -lAFhnU"
