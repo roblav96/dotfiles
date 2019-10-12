c00d53c64c6487f4c615f7e6fcc7c3b0d45e5ca3
Robert Laverty <roblav96@gmail.com>
2019-10-12 04:44:13 -0400

    [Darwin]  M zshrc.Linux.after.sh

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -2,6 +2,11 @@ export PURE_PROMPT_SYMBOL="SERVER ➤"
 
 alias pst="pstree -achls"
 
+if type -w "cln" | grep -q -E alias$; then
+	unalias cln
+	alias cln="sudo ln -sf $HOME/.cargo/bin/* /usr/local/bin"
+fi
+
 # if [[ -x "$(which pm2)" ]]; then
 # 	source <(pm2 completion)
 # fi
