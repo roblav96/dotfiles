422b5ba86728d65a8e50cd4a05219a54099250c9
Robert Laverty <roblav96@gmail.com>
2019-10-13 06:38:59 -0400

    [Darwin]  M modules/tc.sh

--- modules/tc.sh
+++ modules/tc.sh
@@ -2,6 +2,7 @@ alias tc="tc -color -pretty -iec"
 alias tca="command tc -color -pretty -iec -details -statistics"
 
 alias tcg="command tc -color -pretty -iec -graph"
-alias tcag="command tc -color -pretty -iec -details -statistics -graph"
+alias tcga="command tc -color -pretty -iec -graph -details -statistics"
 
-alias tcj="command tc -iec -details -statistics -json"
+alias tcj="command tc -iec -json"
+alias tcja="command tc -iec -json -details -statistics"
