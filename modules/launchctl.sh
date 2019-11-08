1d0129d66315715310eb2cd654893adccf7da1df
Robert Laverty <roblav96@gmail.com>
2019-11-08 01:55:31 -0500

    [Darwin]  M modules/launchctl.sh

--- modules/launchctl.sh
+++ modules/launchctl.sh
@@ -2,5 +2,8 @@ alias lcls="echo '\n⭐ User Agents'; launchctl list; echo '\n⭐ System Daemons
 alias lchostinfo="echo '\n⭐ User Host Info'; launchctl hostinfo; echo '\n⭐ System Host Info'; sudo launchctl hostinfo"
 
 function fpl() {
-	fd --color=always --hidden --no-ignore --fixed-strings "$@" "/System/Library/LaunchAgents" "/System/Library/LaunchDaemons"
+	echo "\n⭐ LaunchAgents"
+	fd --color=always --hidden --no-ignore --fixed-strings "$@" "/System/Library/LaunchAgents" | grep "$@|"
+	echo "\n⭐ LaunchDaemons"
+	fd --color=always --hidden --no-ignore --fixed-strings "$@" "/System/Library/LaunchDaemons" | grep "$@|"
 }
