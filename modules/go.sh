8be3bded035fc97c3a58f51da69855f68d5179f4
Robert Laverty <roblav96@gmail.com>
2019-12-03 02:18:13 -0500

    [Darwin]  M modules/go.sh

--- modules/go.sh
+++ modules/go.sh
@@ -2,7 +2,7 @@
 if test -d "$HOME/.go"; then
 	export GOPATH="$HOME/.go"
 	export GOBIN="$HOME/.go/bin"
-	export PATH="$HOME/.go/bin:$PATH"
+	# export PATH="$HOME/.go/bin:$PATH"
 fi
 # export PATH="/usr/local/opt/glide/bin:$PATH"
 # export GLIDE_HOME="/usr/local/opt/glide"
