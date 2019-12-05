e8c2847a9a544921a7f9178df31cbf772162a2dc
Robert Laverty <roblav96@gmail.com>
2019-12-05 05:06:11 -0500

    [Darwin]  M modules/bat.sh

---
+++ configs/npm-ls-g.sh
@@ -0,0 +1,20 @@
+echo npm install -g \
+	@prettier/plugin-ruby@0.16.0 \
+	@prettier/plugin-xml@0.3.0 \
+	bash-language-server@1.6.1 \
+	javascript-typescript-langserver@2.11.3 \
+	npm@6.13.2 \
+	pnpm@4.3.3 \
+	prettier@1.19.1 \
+	prettier-plugin-sh@0.2.4 \
+	prettier-plugin-toml@0.3.1 \
+	snyk@1.256.0 \
+	terminalizer@0.7.1 \
+	ts-node@8.5.4 \
+	typescript@3.7.3 \
+	typescript-language-server@0.4.0 \
+	vscode-css-languageserver-bin@1.4.0 \
+	vscode-html-languageserver-bin@1.4.0 \
+	vscode-json-languageserver-bin@1.0.1 \
+	vue-language-server@0.0.65 \
+	yaml-language-server@0.6.1
--- modules/bat.sh
+++ modules/bat.sh
@@ -12,10 +12,10 @@ function batplist() {
 }; compdef batplist=cat
 alias batpl="batplist"
 
+local BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc')
+# local BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc' 'asa' 'yasm' 'awk' 'bat' 'bib' 'sh' '.bash_logout' '.zshrc' 'c' 'cs' 'cpp' 'cabal' 'clj' 'CMakeLists' 'h' 'hh' 'CMakeCache' 'cr' 'css' 'd' 'dart' 'diff' 'Dockerfile' '.env' '.env' '.env' 'ex' 'elm' 'erl' 'fs' 'fs' 'fish' 'attributes' 'COMMIT_EDITMSG' 'gitconfig' 'exclude' '.git' 'gitlog' 'git' 'go' 'dot' 'groovy' 'hs' 'hs' 'show' 'hosts' 'html' 'asp' 'html' 'yaws' 'rails' 'adp' 'twig' 'ini' 'java' 'properties' 'jsp' 'js' 'js' 'js' 'json' 'sublime' 'sublime' 'jsonnet' 'jl' 'kt' 'tex' 'less' 'lisp' 'lhs' 'lua' 'make' 'Makefile' 'man' 'md' 'matlab' 'build' 'nix' 'm' 'mm' 'ml' 'mll' 'mly' 'org' 'pas' 'pl' 'php' 'txt' 'ps1' 'proto' 'pb' 'pp' 'purs' 'py' 'Sconstruct' 'R' 'rd' 're' 'requirements' 'rst' 'robot' 'rb' 'Deliverfile' 'podspec' 'simplecov' 'haml' 'rxml' 'rs' 'sass' 'scala' 'scss' 'sql' 'erbsql' 'ssh_config' 'sshd_config' 'strace' 'swift' 'log' 'tcl' 'tf' 'sty' 'textile' 'toml' 'ts' 'tsx' 'varlink' 'v' 'vim' 'xml' 'yaml')
 function batlangs() {
 	echo "🌕 batlangs @ -> $@"
-	# local BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc' 'asa' 'yasm' 'awk' 'bat' 'bib' 'sh' '.bash_logout' '.zshrc' 'c' 'cs' 'cpp' 'cabal' 'clj' 'CMakeLists' 'h' 'hh' 'CMakeCache' 'cr' 'css' 'd' 'dart' 'diff' 'Dockerfile' '.env' '.env' '.env' 'ex' 'elm' 'erl' 'fs' 'fs' 'fish' 'attributes' 'COMMIT_EDITMSG' 'gitconfig' 'exclude' '.git' 'gitlog' 'git' 'go' 'dot' 'groovy' 'hs' 'hs' 'show' 'hosts' 'html' 'asp' 'html' 'yaws' 'rails' 'adp' 'twig' 'ini' 'java' 'properties' 'jsp' 'js' 'js' 'js' 'json' 'sublime' 'sublime' 'jsonnet' 'jl' 'kt' 'tex' 'less' 'lisp' 'lhs' 'lua' 'make' 'Makefile' 'man' 'md' 'matlab' 'build' 'nix' 'm' 'mm' 'ml' 'mll' 'mly' 'org' 'pas' 'pl' 'php' 'txt' 'ps1' 'proto' 'pb' 'pp' 'purs' 'py' 'Sconstruct' 'R' 'rd' 're' 'requirements' 'rst' 'robot' 'rb' 'Deliverfile' 'podspec' 'simplecov' 'haml' 'rxml' 'rs' 'sass' 'scala' 'scss' 'sql' 'erbsql' 'ssh_config' 'sshd_config' 'strace' 'swift' 'log' 'tcl' 'tf' 'sty' 'textile' 'toml' 'ts' 'tsx' 'varlink' 'v' 'vim' 'xml' 'yaml')
-	local BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc')
 	for BAT_LANG in "${BAT_LANGS[@]}"; do
 		echo "🌕 BAT_LANG -> $BAT_LANG"
 		# $@ | bat --style=grid -l "$BAT_LANG"
