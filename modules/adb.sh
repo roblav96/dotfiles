33f5f3942049cb891685edce0d7b0fa3da0b5930
Robert Laverty <roblav96@gmail.com>
2019-12-05 12:36:14 -0500

    [Darwin]  M modules/adb.sh

--- modules/adb.sh
+++ modules/adb.sh
@@ -1,25 +1,32 @@
+#!/usr/bin/env bash
+
+# Android Package Manager
+# https://developer.android.com/studio/command-line/adb#pm
+
 function adb-pm-ls() {
-	echo '\n🌕 Default Packages 🌕'
-	echo "$(adb shell pm list packages -f)" | bat -p -l strace | bat -p -l ini
-	echo '\n🌕 Disabled Packages 🌕'
-	echo "$(adb shell pm list packages -f -d)" | bat -p -l strace | bat -p -l ini
-	echo '\n🌕 Enabled Packages 🌕'
-	echo "$(adb shell pm list packages -f -e)" | bat -p -l strace | bat -p -l ini
-	echo '\n🌕 System Packages 🌕'
-	echo "$(adb shell pm list packages -f -s)" | bat -p -l strace | bat -p -l ini
-	echo '\n🌕 Third-Party Packages 🌕'
-	echo "$(adb shell pm list packages -f -3)" | bat -p -l strace | bat -p -l ini
+	echo; echo 🌕 Disabled 🌕
+	echo "$(adb shell pm list packages -f -d)" | sort --ignore-case | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
+	echo; echo 🌕 Uninstalled 🌕
+	echo "$(adb shell pm list packages -f -u)" | sort --ignore-case | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
+	echo; echo 🌕 Enabled 🌕
+	echo "$(adb shell pm list packages -f -e)" | sort --ignore-case | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
+	echo; echo 🌕 Default 🌕
+	echo "$(adb shell pm list packages -f)" | sort --ignore-case | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
+	echo; echo 🌕 System 🌕
+	echo "$(adb shell pm list packages -f -s)" | sort --ignore-case | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
+	echo; echo 🌕 Third-Party 🌕
+	echo "$(adb shell pm list packages -f -3)" | sort --ignore-case | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
 }
 
-function adb-pm-f() {
-	echo '\n🌕 Default Packages 🌕'
-	echo "$(adb shell pm list packages -f)" | rg -uuu --passthru --smart-case --fixed-strings --color=always $@
-	echo '\n🌕 Disabled Packages 🌕'
-	echo "$(adb shell pm list packages -f -d)" | rg -uuu --passthru --smart-case --fixed-strings --color=always $@
-	echo '\n🌕 Enabled Packages 🌕'
-	echo "$(adb shell pm list packages -f -e)" | rg -uuu --passthru --smart-case --fixed-strings --color=always $@
-	echo '\n🌕 System Packages 🌕'
-	echo "$(adb shell pm list packages -f -s)" | rg -uuu --passthru --smart-case --fixed-strings --color=always $@
-	echo '\n🌕 Third-Party Packages 🌕'
-	echo "$(adb shell pm list packages -f -3)" | rg -uuu --passthru --smart-case --fixed-strings --color=always $@
-}
+# function adb-pm-ls() {
+# 	echo "$(adb shell '
+# 		echo; echo 🌕 Disabled 🌕; pm list packages -f -d;
+# 		echo; echo 🌕 Uninstalled 🌕; pm list packages -f -u;
+# 		echo; echo 🌕 Default 🌕; pm list packages -f;
+# 		echo; echo 🌕 System 🌕; pm list packages -f -s;
+# 		echo; echo 🌕 Enabled 🌕; pm list packages -f -e;
+# 		echo; echo 🌕 Third-Party 🌕; pm list packages -f -3;
+# 	')" | sed --unbuffered --regexp-extended 's/^package://' | bat -p -l properties
+# }
+# alias adb-pm-f="adb-pm-ls | grep"
+# alias adb-pm-f="adb-pm-ls | rg --smart-case --fixed-strings --color=always --passthru"
