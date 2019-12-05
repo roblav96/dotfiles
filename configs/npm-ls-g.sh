b75cd90450eb8457a30d7e502144f23324472faf
Robert Laverty <roblav96@gmail.com>
2019-12-05 05:08:17 -0500

    [Darwin]  M configs/npm-ls-g.sh

--- configs/npm-ls-g.sh
+++ configs/npm-ls-g.sh
@@ -1,20 +1,19 @@
-echo npm install -g \
-	@prettier/plugin-ruby@0.16.0 \
-	@prettier/plugin-xml@0.3.0 \
-	bash-language-server@1.6.1 \
-	javascript-typescript-langserver@2.11.3 \
-	npm@6.13.2 \
-	pnpm@4.3.3 \
-	prettier@1.19.1 \
-	prettier-plugin-sh@0.2.4 \
-	prettier-plugin-toml@0.3.1 \
-	snyk@1.256.0 \
-	terminalizer@0.7.1 \
-	ts-node@8.5.4 \
-	typescript@3.7.3 \
-	typescript-language-server@0.4.0 \
-	vscode-css-languageserver-bin@1.4.0 \
-	vscode-html-languageserver-bin@1.4.0 \
-	vscode-json-languageserver-bin@1.0.1 \
-	vue-language-server@0.0.65 \
-	yaml-language-server@0.6.1
+echo @prettier/plugin-ruby@0.16.0
+echo @prettier/plugin-xml@0.3.0
+echo bash-language-server@1.6.1
+echo javascript-typescript-langserver@2.11.3
+echo npm@6.13.2
+echo pnpm@4.3.3
+echo prettier@1.19.1
+echo prettier-plugin-sh@0.2.4
+echo prettier-plugin-toml@0.3.1
+echo snyk@1.256.0
+echo terminalizer@0.7.1
+echo ts-node@8.5.4
+echo typescript@3.7.3
+echo typescript-language-server@0.4.0
+echo vscode-css-languageserver-bin@1.4.0
+echo vscode-html-languageserver-bin@1.4.0
+echo vscode-json-languageserver-bin@1.0.1
+echo vue-language-server@0.0.65
+echo yaml-language-server@0.6.1
