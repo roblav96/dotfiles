d74398a5e8a7531e85c2c585a852805eb7f5c232
Robert Laverty <roblav96@gmail.com>
2019-12-08 08:32:46 -0500

    [Darwin]  M bashrc.sh

--- modules/speed-test.sh
+++ modules/speed-test.sh
@@ -25,7 +25,7 @@ echo;
 echo '\t\tipinfo.io';
 curl https://ipinfo.io | json '. |= del(.readme)'
 "
-alias cachefly="wget -O /dev/null https://cachefly.cachefly.net/100mb.test"
-alias ie8vbox="wget -O /dev/null https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE8/IE8.Win7.VirtualBox.zip"
-alias linode="wget -O /dev/null http://speedtest.newark.linode.com/100MB-newark.bin"
+alias cachefly="wget --quiet --show-progress -O /dev/null https://cachefly.cachefly.net/100mb.test"
+alias ie8vbox="wget --quiet --show-progress -O /dev/null https://az792536.vo.msecnd.net/vms/VMBuild_20150916/VirtualBox/IE8/IE8.Win7.VirtualBox.zip"
+alias linode="wget --quiet --show-progress -O /dev/null http://speedtest.newark.linode.com/100MB-newark.bin"
 alias iperf="iperf3 -c 192.34.85.234 -p 15201 --verbose"
