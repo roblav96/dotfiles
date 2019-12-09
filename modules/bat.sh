6df07962cc161018d252d9565ef42338881af884
Robert Laverty <roblav96@gmail.com>
2019-12-08 23:37:00 -0500

    [Darwin]  M modules/bat.sh

--- modules/bat.sh
+++ modules/bat.sh
@@ -1,7 +1,7 @@
 export BAT_FLAGS="--color=always --italic-text=always --decorations=always --tabs=4 --paging=never --wrap=never --theme='Monokai Extended Origin'"
 # export BAT_CONFIG_PATH="$DOTFILES/static"
 
-alias bat="command bat $BAT_FLAGS --style=header,grid"
+alias bat="bat $BAT_FLAGS --style=header,grid"
 alias batt="command bat $BAT_FLAGS --style=header,grid,numbers"
 alias b="bat"
 alias bb="batt"
@@ -13,19 +13,12 @@ function batplist() {
 }; compdef batplist=cat
 alias batpl="batplist"
 
-export BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc')
-# local BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc' 'asa' 'yasm' 'awk' 'bat' 'bib' 'sh' '.bash_logout' '.zshrc' 'c' 'cs' 'cpp' 'cabal' 'clj' 'CMakeLists' 'h' 'hh' 'CMakeCache' 'cr' 'css' 'd' 'dart' 'diff' 'Dockerfile' '.env' '.env' '.env' 'ex' 'elm' 'erl' 'fs' 'fs' 'fish' 'attributes' 'COMMIT_EDITMSG' 'gitconfig' 'exclude' '.git' 'gitlog' 'git' 'go' 'dot' 'groovy' 'hs' 'hs' 'show' 'hosts' 'html' 'asp' 'html' 'yaws' 'rails' 'adp' 'twig' 'ini' 'java' 'properties' 'jsp' 'js' 'js' 'js' 'json' 'sublime' 'sublime' 'jsonnet' 'jl' 'kt' 'tex' 'less' 'lisp' 'lhs' 'lua' 'make' 'Makefile' 'man' 'md' 'matlab' 'build' 'nix' 'm' 'mm' 'ml' 'mll' 'mly' 'org' 'pas' 'pl' 'php' 'txt' 'ps1' 'proto' 'pb' 'pp' 'purs' 'py' 'Sconstruct' 'R' 'rd' 're' 'requirements' 'rst' 'robot' 'rb' 'Deliverfile' 'podspec' 'simplecov' 'haml' 'rxml' 'rs' 'sass' 'scala' 'scss' 'sql' 'erbsql' 'ssh_config' 'sshd_config' 'strace' 'swift' 'log' 'tcl' 'tf' 'sty' 'textile' 'toml' 'ts' 'tsx' 'varlink' 'v' 'vim' 'xml' 'yaml')
+# export BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc')
+export BAT_LANGS=('as' 'csv' 'applescript' 's' 'adoc' 'asa' 'yasm' 'awk' 'bat' 'bib' 'sh' '.bash_logout' '.zshrc' 'c' 'cs' 'cpp' 'cabal' 'clj' 'CMakeLists' 'h' 'hh' 'CMakeCache' 'cr' 'css' 'd' 'dart' 'diff' 'Dockerfile' '.env' '.env' '.env' 'ex' 'elm' 'erl' 'fs' 'fs' 'fish' 'attributes' 'COMMIT_EDITMSG' 'gitconfig' 'exclude' '.git' 'gitlog' 'git' 'go' 'dot' 'groovy' 'hs' 'hs' 'show' 'hosts' 'html' 'asp' 'html' 'yaws' 'rails' 'adp' 'twig' 'ini' 'java' 'properties' 'jsp' 'js' 'js' 'js' 'json' 'sublime' 'sublime' 'jsonnet' 'jl' 'kt' 'tex' 'less' 'lisp' 'lhs' 'lua' 'make' 'Makefile' 'man' 'md' 'matlab' 'build' 'nix' 'm' 'mm' 'ml' 'mll' 'mly' 'org' 'pas' 'pl' 'php' 'txt' 'ps1' 'proto' 'pb' 'pp' 'purs' 'py' 'Sconstruct' 'R' 'rd' 're' 'requirements' 'rst' 'robot' 'rb' 'Deliverfile' 'podspec' 'simplecov' 'haml' 'rxml' 'rs' 'sass' 'scala' 'scss' 'sql' 'erbsql' 'ssh_config' 'sshd_config' 'strace' 'swift' 'log' 'tcl' 'tf' 'sty' 'textile' 'toml' 'ts' 'tsx' 'varlink' 'v' 'vim' 'xml' 'yaml')
 function batlangs() {
-	echo "🌕 batlangs"
-	local out=""
-	while read data; do
-		out+="$data"
+	local data=$(</dev/stdin)
+	for BAT_LANG in "${BAT_LANGS[@]}"; do
+		echo "🌕 $BAT_LANG"
+		echo $data | bat --style=header,grid,numbers -l "$BAT_LANG"
 	done
-	echo "🌕 out -> $out"
-	# echo "🌕 # -> $($#)"
-	# echo "🌕 @ -> $($@)"
-	# for BAT_LANG in "${BAT_LANGS[@]}"; do
-	# 	echo "🌕 BAT_LANG -> $BAT_LANG"
-	# 	# $@ | bat --style=grid -l "$BAT_LANG"
-	# done
 }
---
+++ modules/osascript.sh
@@ -0,0 +1,2 @@
+alias mark="osascript -e 'if app \"Terminal\" is frontmost then tell app \"System Events\" to keystroke \"u\" using command down'"
+alias bookmark="osascript -e 'if app \"Terminal\" is frontmost then tell app \"System Events\" to keystroke \"M\" using command down'"
