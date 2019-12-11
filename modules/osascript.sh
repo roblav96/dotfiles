aaa0281aaaf94cf20eb52e8d6973238a8b210a04
Robert Laverty <roblav96@gmail.com>
2019-12-11 05:19:54 -0500

    [Darwin]  M modules/osascript.sh

--- modules/osascript.sh
+++ modules/osascript.sh
@@ -1,2 +1,3 @@
+alias newtab="osascript -e 'if app \"Terminal\" is frontmost then tell app \"System Events\" to keystroke \"n\" using command down'"
 alias mark="osascript -e 'if app \"Terminal\" is frontmost then tell app \"System Events\" to keystroke \"u\" using command down'"
 alias bookmark="osascript -e 'if app \"Terminal\" is frontmost then tell app \"System Events\" to keystroke \"M\" using command down'"
