522c3a3bd28a51ddbcbb4cb7e15080e38c6d5878
Robert Laverty <roblav96@gmail.com>
2019-12-14 18:01:49 -0500

    [Darwin]  M modules/sublime-text.sh

--- modules/sublime-text.sh
+++ modules/sublime-text.sh
@@ -2,14 +2,11 @@ alias st="subl"
 alias sm="smerge"
 alias stp="subl --new-window --command 'project_manager {\"action\": \"switch\"}'"
 alias stcd="cd '$HOME/Library/Application Support/Sublime Text 3'"
-alias stuser="subl '$HOME/Library/Application Support/Sublime Text 3/Packages/User'"
-alias stprojects="subl '$HOME/Library/Application Support/Sublime Text 3/Packages/User/Projects'"
-alias stsnippets="subl '$HOME/Library/Application Support/Sublime Text 3/Packages/User/Snippets'"
-alias stpackages="subl '$HOME/Library/Application Support/Sublime Text 3/Packages/User/Projects/Sublime Text 3 Packages.sublime-project'"
+alias stcdu="cd '$HOME/Library/Application Support/Sublime Text 3/Packages/User'"
 alias stbak="zip -r $HOME/Downloads/st3-backup.\$(date --iso-8601).zip '$HOME/Library/Application Support/Sublime Text 3'"
 
 function stpush() {
-	stcd
+	cd "$HOME/Library/Application Support/Sublime Text 3"
 	local gs="$(git status -z)"
 	git add -A
 	git commit -a -m "[`uname -o`] $gs"
