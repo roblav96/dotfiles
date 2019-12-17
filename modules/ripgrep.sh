b7704b3e09f33747d5094246df1fa9cddc3c1a58
Robert Laverty <roblav96@gmail.com>
2019-12-17 04:58:46 -0500

    [Darwin]  M modules/ripgrep.sh

--- modules/ripgrep.sh
+++ modules/ripgrep.sh
@@ -2,8 +2,7 @@ export RG_FLAGS="--pretty --trim --no-messages --smart-case --hidden --fixed-str
 # export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 
 alias r="rg $RG_FLAGS --glob='!node_modules'"
-alias ra="rg $RG_FLAGS --no-ignore --stats"
-alias raa="rg $RG_FLAGS --no-ignore --stats --follow --text --search-zip -uuu"
+alias ra="rg $RG_FLAGS --no-ignore --stats --follow --text --search-zip -uuu"
 alias rgp="rg --passthru --ignore-case --fixed-strings"
 # alias raf="rg $RG_FLAGS --no-ignore --fixed-strings --stats --follow"
 # alias r="rg --color=always --smart-case"
