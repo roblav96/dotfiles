9b924a20b486a4573ccc375bdfc86323892d7a2a
Robert Laverty <roblav96@gmail.com>
2020-01-21 00:00:32 -0500

    [Darwin]  M modules/speed-test.sh

--- modules/speed-test.sh
+++ modules/speed-test.sh
@@ -21,5 +21,4 @@ function ipinfo() {
 	fi
 }
 
-# alias ipinfo="echo; curl https://icanhazip.com; curl https://api6.ipify.org; echo; echo; curl https://ipinfo.io | json '. |= del(.readme)'"
-# alias ipinfoa="echo; echo 🌕 icanhazip.com; curl https://icanhazip.com; echo; echo 🌕 api6.ipify.org; curl https://api6.ipify.org; echo; echo; echo 🌕 iplist.cc; curl https://iplist.cc/api | json; echo; echo 🌕 ipvigilante.com; curl https://ipvigilante.com/json/full | json '.data'; echo; echo 🌕 ifconfig.co; curl https://ifconfig.co/json | json '. |= del(.user_agent)'; echo; echo 🌕 ipinfo.io; curl https://ipinfo.io | json '. |= del(.readme)'"
+alias ipinfoa="echo; echo 🌕 icanhazip.com; curl https://icanhazip.com; echo; echo 🌕 api6.ipify.org; curl https://api6.ipify.org; echo; echo; echo 🌕 iplist.cc; curl https://iplist.cc/api | json; echo; echo 🌕 ifconfig.co; curl https://ifconfig.co/json | json '. |= del(.user_agent)'; echo; echo 🌕 ipinfo.io; curl https://ipinfo.io | json '. |= del(.readme)'"
