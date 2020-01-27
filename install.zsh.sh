3d16ba5ded1e98df21471fed79e58c405a46a50a
Robert Laverty <roblav96@gmail.com>
2020-01-27 16:07:41 -0500

    [Darwin]  M install.zsh.sh

--- install.zsh.sh
+++ install.zsh.sh
@@ -7,7 +7,7 @@ echo > $ZSHRC
 echo 'export PLATFORM="$(uname -o)"' >> $ZSHRC
 echo "export DOTFILES='$DOTFILES'" >> $ZSHRC
 echo "alias src='sh $DOTFILES/install.zsh.sh; rm -v $(dirname $DOTFILES)/.zcomp*; compinit; exit'" >> $ZSHRC
-echo "test ! -d ~/.zinit/bin && mkdir ~/.zinit; rm -rf ~/.zinit/bin; git clone git@github.com:zdharma/zinit.git ~/.zinit/bin" >> $ZSHRC
+echo "test ! -d ~/.zinit/bin && mkdir ~/.zinit && rm -rf ~/.zinit/bin && git clone git@github.com:zdharma/zinit.git ~/.zinit/bin" >> $ZSHRC
 echo "source ~/.zinit/bin/zinit.zsh" >> $ZSHRC
 echo "[ -f '$DOTFILES/zshrc.${PLATFORM##*/}.before.sh' ] && source '$DOTFILES/zshrc.${PLATFORM##*/}.before.sh'" >> $ZSHRC
 echo "[ -f '$DOTFILES/zshrc.sh' ] && source '$DOTFILES/zshrc.sh'" >> $ZSHRC
