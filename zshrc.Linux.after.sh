fedee971ac0e59b880192723dffec77bc0eadb42
Robert Laverty <roblav96@gmail.com>
2020-02-18 02:15:54 -0500

    [Darwin]  M zshrc.Linux.after.sh

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -1,19 +1,19 @@
 export PURE_PROMPT_SYMBOL="SERVER ➤"
 
 export SYSTEMD_PAGER=""
-unalias sc-status
+unalias sc-status &>/dev/null
 alias systemctl="systemctl --no-pager"
 alias sc-status="systemctl --full status"
 
 alias pst="pstree -achls"
 
 if which -w cglnbin | grep -q "alias$"; then
-	unalias cglnbin
+	unalias cglnbin &>/dev/null
 	alias cglnbin="sudo ln -sf $HOME/.cargo/bin/* /usr/local/bin"
 fi
 
 if which -w golnbin | grep -q "alias$"; then
-	unalias golnbin
+	unalias golnbin &>/dev/null
 	alias golnbin="sudo ln -sf $GOBIN/* /usr/local/bin"
 fi
 
