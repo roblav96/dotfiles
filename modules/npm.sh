636e7d932237612e229995e18fd6795e727fcff5
Robert Laverty <roblav96@gmail.com>
2020-02-28 13:54:56 -0500

    [Darwin]  M modules/npm.sh

--- modules/npm.sh
+++ modules/npm.sh
@@ -5,7 +5,7 @@ alias npmlsa="npm --silent ls"
 alias npmo="npm outdated --long"
 
 alias snykt="snyk test --dev --all-projects --detection-depth=1"
-alias npmi='test -x "$(which -p snyk)" && snykt && read -q "?npm install? [y/n]: " || return 1; npm install'
+alias npmi='rm -f package-lock.json; npm install --ignore-scripts --no-bin-links --package-lock-only; snykt; rm -f package-lock.json; read -q "?npm install? [y/n]: " || return 1; npm install'
 
 function npmin() {
 	npm install "$@" && npm install -D "@types/$@"
