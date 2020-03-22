b387c786d4dbec9e0eaea50687649d8d006dde25
Robert Laverty <roblav96@gmail.com>
2020-03-22 17:43:37 -0400

    [Darwin]  M modules/nix.sh

--- modules/nix.sh
+++ modules/nix.sh
@@ -5,6 +5,7 @@ unalias ns &>/dev/null
 
 alias nin="nix-env --install"
 alias nls="nix-env --query"
+alias nout="nix-env -qc"
 alias nrm="nix-env --uninstall"
 alias ns="nix search"
 alias nupd="nix search --update-cache"
