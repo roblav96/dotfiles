3d59beb447d1864c91b56cfa9a74b1e2dfd95f00
Robert Laverty <roblav96@gmail.com>
2020-03-28 21:26:23 -0400

    [Darwin]  M modules/tldr.sh

--- modules/tldr.sh
+++ modules/tldr.sh
@@ -4,18 +4,28 @@ alias tlls="tldr --list"
 alias tls="tldr --list | sed --unbuffered --regexp-extended 's/, /\n/g' | grep"
 
 function how() {
-	local output="$(howdoi --color --all "$@")"
+	local output="$(howdoi --color --all --num-answers=3 "$@")"
 	for i in "${@}"; do
 		output="$(echo "$output" | rgp "$i")"
 	done
 	echo "$output"
+	#
+	# # echo; echo "🌕 $(howdoi --all --link "$@")"; echo
+	# local output=""
+	# for i in {1..3}; do
+	# 	output="$output\n🌕 $i $(howdoi --num-answers=3 --pos=$i --link "$@")"
+	# 	output="$output\n$(howdoi --num-answers=3 --pos=$i "$@")"
+	# 	output="$output\n$(howdoi --num-answers=3 --pos=$i --all "$@")"
+	# done
+	# echo "$output"
+	#
 	# local regexp="$(echo "$@" | sd ' ' '|')"
 	# local regexp="($(echo "$@" | sed 's/ /|/g'))"
 	# local regexp="${@//\\s/|}"
 	# echo "🌕 regexp -> '$regexp'"
 	# howdoi --color --all "$@" | rg --color=always --ignore-case --passthru --regexp="'"$regexp"'"
 }
-# alias how="howdoi --color --all"
+alias hdi="howdoi --color --all"
 # alias hdi='function _hdi() { howdoi --color --all $@ }; _hdi'
 
 function ch() {
