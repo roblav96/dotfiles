d25d4ff5c416b42e175b9ac1e72c54803a96af4e
Robert Laverty <roblav96@gmail.com>
2020-03-28 20:37:35 -0400

    [Darwin]  M modules/tldr.sh

--- modules/tldr.sh
+++ modules/tldr.sh
@@ -3,7 +3,20 @@ alias tlupd="tldr --update"
 alias tlls="tldr --list"
 alias tls="tldr --list | sed --unbuffered --regexp-extended 's/, /\n/g' | grep"
 
-alias how="howdoi --color --all"
+function how() {
+	local output="$(howdoi --color --all "$@")"
+	for i in "${@}"; do
+		output="$(echo "$output" | rgp "$i")"
+	done
+	echo "$output"
+	# local regexp="$(echo "$@" | sd ' ' '|')"
+	# local regexp="($(echo "$@" | sed 's/ /|/g'))"
+	# local regexp="${@//\\s/|}"
+	# echo "🌕 regexp -> '$regexp'"
+	# howdoi --color --all "$@" | rg --color=always --ignore-case --passthru --regexp="'"$regexp"'"
+}
+# alias how="howdoi --color --all"
+# alias hdi='function _hdi() { howdoi --color --all $@ }; _hdi'
 
 function ch() {
 	local lang="sh"
