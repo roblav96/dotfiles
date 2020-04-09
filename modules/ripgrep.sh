8a842549eb7f98eb33987a8e8eca244c3366fe43
Robert Laverty <roblav96@gmail.com>
2020-04-08 21:54:43 -0400

    [Darwin]  M modules/ripgrep.sh

--- modules/ripgrep.sh
+++ modules/ripgrep.sh
@@ -1,4 +1,5 @@
 export RG_FLAGS="--color=always --heading --line-number --trim --no-messages --smart-case --hidden --fixed-strings --glob='!.git' --glob='!.DS_Store'"
+# --colors=match:bg:16 --colors=path:bg:16
 # --colors=path:fg:green --colors=path:style:bold
 export RG_MAX_COLUMNS_FLAGS="--max-columns-preview --max-columns=\$(expr \$(tput cols) - 25)"
 # export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
--- zshrc.sh
+++ zshrc.sh
@@ -153,8 +153,10 @@ alias archey="archey --offline"
 # alias ll="ls -lAFhnU"
 # alias man="man -P more"
 
-export GREP_COLOR="01;31;48;5;16"
-export GREP_COLORS="ms=01;31;48;5;16:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36"
+# export GREP_COLOR="01;31;48;5;16"
+# export GREP_COLORS="ms=01;31;48;5;16:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36"
+export GREP_COLOR="01;31"
+export GREP_COLORS="ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36"
 alias grep="grep --color=always --ignore-case --extended-regexp"
 alias g="grep"
 
