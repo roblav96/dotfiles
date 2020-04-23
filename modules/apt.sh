d651c6020905347a17f0b18af6423a25f29fcfa9
Robert Laverty <roblav96@gmail.com>
2020-04-22 21:18:28 -0400

    [Darwin]  M modules/apt.sh

--- modules/apt.sh
+++ modules/apt.sh
@@ -1,8 +1,6 @@
 alias aptupd="sudo apt update"
 alias aptupg="sudo apt upgrade"
 alias apti="apt show"
-alias aptls="apt list"
-alias aptlsi="apt list --installed"
 alias aptfs="apt-file list"
 
 alias dpkgi="dpkg-query --status"
@@ -11,4 +9,10 @@ alias dpkgfs="dpkg-query --listfiles"
 # alias apts="apt search"
 function apts() {
 	apt search $@ 2>/dev/null | rgp $@
+}
+function aptls() {
+	apt list 2>/dev/null
+}
+function aptlsi() {
+	apt list --installed 2>/dev/null
 }
