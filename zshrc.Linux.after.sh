46cb5f3934d14f6ccbed92cfb68d6fce2753b57a
Robert Laverty <roblav96@gmail.com>
2020-04-22 23:47:05 -0400

    [Darwin]  M zshrc.Linux.after.sh

--- zshrc.Linux.after.sh
+++ zshrc.Linux.after.sh
@@ -12,15 +12,17 @@ alias sc-status="systemctl --full status"
 
 alias pst="pstree -achls"
 
-if which -w cglnbin | grep -q "alias$"; then
-	unalias cglnbin &>/dev/null
-	alias cglnbin="sudo ln -sf $HOME/.cargo/bin/* /usr/local/bin"
-fi
+alias nixln="sudo ln -sf $HOME/.nix-profile/bin/* /usr/local/bin"
 
-if which -w golnbin | grep -q "alias$"; then
-	unalias golnbin &>/dev/null
-	alias golnbin="sudo ln -sf $GOBIN/* /usr/local/bin"
-fi
+# if which -w cglnbin | grep -q "alias$"; then
+# 	unalias cglnbin &>/dev/null
+# 	alias cglnbin="sudo ln -sf $HOME/.cargo/bin/* /usr/local/bin"
+# fi
+
+# if which -w golnbin | grep -q "alias$"; then
+# 	unalias golnbin &>/dev/null
+# 	alias golnbin="sudo ln -sf $GOBIN/* /usr/local/bin"
+# fi
 
 # if [[ -x "$(which pm2)" ]]; then
 # 	source <(pm2 completion)
