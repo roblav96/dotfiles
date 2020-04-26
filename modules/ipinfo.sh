6f404a46bf1c2c3990650bea1dc40c609b4d6dd5
Robert Laverty <roblav96@gmail.com>
2020-04-26 03:00:39 -0400

    [Darwin]  M modules/ipinfo.sh

--- modules/ipinfo.sh
+++ modules/ipinfo.sh
@@ -12,7 +12,11 @@ function ipinfo() {
 			curl "https://geo.ipify.org/api/v1?apiKey=$_IPIFY_API_KEY" | json
 		fi
 	elif [[ $@ =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-		curl "https://geo.ipify.org/api/v1?apiKey=$_IPIFY_API_KEY&ipAddress=$@" | json
+		if [[ -z "$_IPIFY_API_KEY" ]]; then
+			curl "https://ipinfo.io/$@" | json '. |= del(.readme)'
+		else
+			curl "https://geo.ipify.org/api/v1?apiKey=$_IPIFY_API_KEY&ipAddress=$@" | json
+		fi
 	else
 		curl "https://geo.ipify.org/api/v1?apiKey=$_IPIFY_API_KEY&domain=$@" | json
 	fi
