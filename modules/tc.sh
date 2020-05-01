6e17962a0f5fab5e8edfe51e8f7d4b1d99c8d7be
Robert Laverty <roblav96@gmail.com>
2020-05-01 17:41:07 -0400

    [Darwin]  M modules/tc.sh

--- modules/tc.sh
+++ modules/tc.sh
@@ -1,8 +1,8 @@
-alias tc="tc -color -pretty -iec"
-alias tca="command tc -color -pretty -iec -details -statistics"
+alias tc="tc -color -pretty"
+alias tca="command tc -color -pretty -details -statistics"
 
-alias tcg="command tc -color -pretty -iec -graph"
-alias tcga="command tc -color -pretty -iec -graph -details -statistics"
+alias tcg="command tc -color -pretty -graph"
+alias tcga="command tc -color -pretty -graph -details -statistics"
 
-alias tcj="command tc -iec -json"
-alias tcja="command tc -iec -json -details -statistics"
+alias tcj="command tc -json"
+alias tcja="command tc -json -details -statistics"
