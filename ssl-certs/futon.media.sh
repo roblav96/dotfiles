db040d35d1bd4136cd24b049f98547a16b88179f
Robert Laverty <roblav96@gmail.com>
2020-05-03 11:11:25 -0400

    [Darwin]  M ssl-certs/futon.media.sh

--- ssl-certs/futon.media.sh
+++ ssl-certs/futon.media.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-lego -d futon.media -a -m roblav96@gmx.com --http.webroot /var/www --path /root/.lego --http renew
+lego --path /etc/lego/.lego --accept-tos --email roblav96@gmx.com --domains futon.media --http --http.webroot /var/www renew
+# lego -d futon.media -a -m roblav96@gmx.com --http.webroot /var/www --path /root/.lego --http renew
 # lego -s https://acme-staging-v02.api.letsencrypt.org/directory
 
 # openssl pkcs12 -export -inkey /root/.lego/certificates/futon.media.key -in /root/.lego/certificates/futon.media.crt -certfile /root/.lego/certificates/futon.media.issuer.crt -out /opt/emby-server/etc/ssl/certs/futon.media.pfx -passout pass:
