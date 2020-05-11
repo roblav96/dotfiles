6115f672cb8f07c752d0cebcec102cd31a6e0c0a
Robert Laverty <roblav96@gmail.com>
2020-05-11 19:05:30 -0400

    [Darwin]  M zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -161,7 +161,7 @@ alias archey="archey --offline"
 alias dotcd="cd $DOTFILES"
 alias src="zsh $DOTFILES/install.zsh && zreload"
 alias zbak="sudo cp $HOME/.zsh_history $HOME/..zsh_history; sudo cp $HOME/.z $HOME/..z; [[ -n \$ZSH_COMPLETION_GENERATOR_DIR ]] && ls -1 \$ZSH_COMPLETION_GENERATOR_DIR | sort > $HOME/..gencomp"
-alias zreload="[[ -n "$SHELL" ]] && exec ${SHELL#-} || exec $(command -v zsh)"
+alias zreload="echo; echo '✅ ZSH reload'; [[ -n "$SHELL" ]] && exec ${SHELL#-} || exec $(command -v zsh)"
 alias zcomp="rm -fv $HOME/.zcomp* && zreload"
 alias abupd="antibody update && zcomp"
 alias abautosuggestions="bat \$(antibody home)/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.zsh --line-range=320:324; echo :322" # --highlight-line=322
@@ -332,7 +332,7 @@ function dotcompinit() {
 	local zcompdump_metadata="$(command grep '^#omz' "$ZSH_COMPDUMP" 2>/dev/null)"
 	compinit -d "${ZSH_COMPDUMP}"
 	echo "\n$zcompdump_metadata" >>! "$ZSH_COMPDUMP"
-	echo; echo "✅ dotcompinit"
+	echo; echo "✅ ZSH compinit"
 }
 
 # compinit -d "${ZSH_COMPDUMP}"
