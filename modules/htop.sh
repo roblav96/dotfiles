c6f52d908061840e70e910b623d17c5202caed69
Robert Laverty <roblav96@gmail.com>
2020-05-25 13:51:41 -0400

    [Darwin] M  modules/htop.sh

--- modules/htop.sh
+++ modules/htop.sh
@@ -3,5 +3,5 @@ alias htopcpu="htop --sort-key=PERCENT_CPU"
 alias htopstart="htop --sort-key=STARTTIME"
 alias htoptime="htop --sort-key=TIME"
 
-alias procs="procs --color always --pager disable --sorta Start"
+alias procss="procs --color always --pager disable --sorta Start"
 # test -x "$(which -p procs)" && alias procs="sudo procs"
