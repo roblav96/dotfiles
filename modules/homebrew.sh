56e87a088a97df6fa7f43021034ece802a237abb
Robert Laverty <roblav96@gmail.com>
2020-05-25 06:19:35 -0400

    [Darwin] M  modules/homebrew.sh

--- modules/homebrew.sh
+++ modules/homebrew.sh
@@ -48,7 +48,7 @@ function bin() {
 function bcin() {
 	for v in "$@"; do
 		echo; echo "🌕 Installing cask -> '$v'"
-		brew cask install "$v"
+		brew cask install --no-quarantine "$v"
 	done
 }
 
@@ -62,7 +62,7 @@ function brein() {
 function bcrein() {
 	for v in "$@"; do
 		echo; echo "🌕 Reinstalling cask -> '$v'"
-		brew cask reinstall "$v"
+		brew cask reinstall --no-quarantine "$v"
 	done
 }; compdef bcrein=command
 
