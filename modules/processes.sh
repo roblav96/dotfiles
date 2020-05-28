648f74b1c0cefc075532fb1302dae7206270db6b
Robert Laverty <roblav96@gmail.com>
2020-05-27 23:14:44 -0400

    [Darwin] M  configs/procs.toml

--- modules/processes.sh
+++ modules/processes.sh
@@ -3,5 +3,5 @@ alias htopcpu="htop --sort-key=PERCENT_CPU"
 alias htopstart="htop --sort-key=STARTTIME"
 alias htoptime="htop --sort-key=TIME"
 
-alias procss="procs --color always --pager disable --sorta Start"
+alias procss="procs --sorta Start"
 # test -x "$(which -p procs)" && alias procs="sudo procs"
