c173beceb2be9f00079d76dcfe50ab9c3c762347
Robert Laverty <roblav96@gmail.com>
2020-05-27 21:28:54 -0400

    [Darwin] M  macosx/applications.catalina.sh

--- macosx/applications.catalina.sh
+++ macosx/applications.catalina.sh
@@ -34,15 +34,15 @@ APPLICATIONS=(
 )
 
 for APPLICATION in "${APPLICATIONS[@]}"; do
-	if test -d "./Applications/${APPLICATION}.app"; then
-		chmod 000 "./Applications/${APPLICATION}.app"
-		mv -f "./Applications/${APPLICATION}.app" "./Applications/.${APPLICATION}.app"
-		echo "👍 [DISABLED] Applications -> '${APPLICATION}'"
-	fi
 	if test -d "./System/Applications/${APPLICATION}.app"; then
 		chmod 000 "./System/Applications/${APPLICATION}.app"
 		mv -f "./System/Applications/${APPLICATION}.app" "./System/Applications/.${APPLICATION}.app"
 		echo "👍 [DISABLED] System Applications -> '${APPLICATION}'"
+	fi
+	if test -d "./Applications/${APPLICATION}.app"; then
+		chmod 000 "./Applications/${APPLICATION}.app"
+		mv -f "./Applications/${APPLICATION}.app" "./Applications/.${APPLICATION}.app"
+		echo "👍 [DISABLED] Applications -> '${APPLICATION}'"
 	fi
 done
 
@@ -51,14 +51,14 @@ CORE_SERVICES=(
 )
 
 for CORE_SERVICE in "${CORE_SERVICES[@]}"; do
-	if test -d "./System/Library/CoreServices/${CORE_SERVICE}.app"; then
-		chmod 000 "./System/Library/CoreServices/${CORE_SERVICE}.app"
-		mv -f "./System/Library/CoreServices/${CORE_SERVICE}.app" "./Library/Apple/System/Library/CoreServices/.${CORE_SERVICE}.app"
-		echo "👍 [DISABLED] System Library CoreServices -> '${CORE_SERVICE}'"
-	fi
 	if test -d "./Library/Apple/System/Library/CoreServices/${CORE_SERVICE}.app"; then
 		chmod 000 "./Library/Apple/System/Library/CoreServices/${CORE_SERVICE}.app"
 		mv -f "./Library/Apple/System/Library/CoreServices/${CORE_SERVICE}.app" "./Library/Apple/System/Library/CoreServices/.${CORE_SERVICE}.app"
 		echo "👍 [DISABLED] Library Apple System Library CoreServices -> '${CORE_SERVICE}'"
+	fi
+	if test -d "./System/Library/CoreServices/${CORE_SERVICE}.app"; then
+		chmod 000 "./System/Library/CoreServices/${CORE_SERVICE}.app"
+		mv -f "./System/Library/CoreServices/${CORE_SERVICE}.app" "./System/Library/CoreServices/.${CORE_SERVICE}.app"
+		echo "👍 [DISABLED] System Library CoreServices -> '${CORE_SERVICE}'"
 	fi
 done
