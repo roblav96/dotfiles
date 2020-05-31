97c3ea4835beb1ed7ea6679b7620be3c51eaa13f
Robert Laverty <roblav96@gmail.com>
2020-05-31 06:10:34 -0400

    [Darwin] M  modules/fdfind.sh

--- modules/fdfind.sh
+++ modules/fdfind.sh
@@ -3,16 +3,16 @@ local FD_FLAGS="--color=always --hidden --exclude=.git --exclude=.DS_Store"
 alias f="fd $FD_FLAGS --exclude=node_modules --fixed-strings"
 alias fgl="fd $FD_FLAGS --exclude=node_modules --glob"
 
-alias fpathf="fd $FD_FLAGS --no-ignore --fixed-strings --exact-depth=1 --absolute-path --base-directory=/ \$(printf \"--search-path %q \" \"\${fpath[@]}\")"
-
-local FD_FLAGS_ALL="$FD_FLAGS --no-ignore --full-path"
+local FD_FLAGS_ALL="$FD_FLAGS --no-ignore"
 
 alias fa="fd $FD_FLAGS_ALL --fixed-strings"
-alias faa="fd $FD_FLAGS_ALL --fixed-strings --follow"
+alias faa="fd $FD_FLAGS_ALL --fixed-strings --follow --full-path --absolute-path"
 alias fagl="fd $FD_FLAGS_ALL --glob"
-alias faagl="fd $FD_FLAGS_ALL --glob --follow"
+alias faagl="fd $FD_FLAGS_ALL --glob --follow --full-path --absolute-path"
 
-local fr="fd $FD_FLAGS_ALL --fixed-strings --absolute-path --base-directory=/ \
+alias fpathf="fd $FD_FLAGS_ALL --fixed-strings --exact-depth=1 --absolute-path --base-directory=/ \$(printf \"--search-path %q \" \"\${fpath[@]}\")"
+
+local fr="fd $FD_FLAGS_ALL --fixed-strings --full-path --absolute-path --base-directory=/ \
 --exclude=$HOME/.cargo \
 --exclude=$HOME/.gradle \
 --exclude=$HOME/.node-gyp \
