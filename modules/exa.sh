eb535a8a589f6ff054e2f2c5dbdc31e3082f03c8
Robert Laverty <roblav96@gmail.com>
2020-06-07 09:58:46 -0400

    [Darwin] M  configs/logrc

--- modules/exa.sh
+++ modules/exa.sh
@@ -2,7 +2,7 @@ export EXA_COLORS="uu=2;37:gu=2;3;37:da=32:un=31:gn=2;3;31:lc=2;3;37:lm=3;37"
 # export EXA_STRICT="1"
 
 local EXA_FLAGS="--long --all --header --classify --group --color-scale --color=always"
-realpath "$(which -p exa)" | grep -q 'HEAD' && EXA_FLAGS="$EXA_FLAGS --icons"
+# realpath "$(which -p exa)" | grep -q 'HEAD' && EXA_FLAGS="$EXA_FLAGS --icons"
 
 alias la="exa $EXA_FLAGS --extended --links --tree --level=1"
 alias lar="exa $EXA_FLAGS --extended --links --tree --level=2"
