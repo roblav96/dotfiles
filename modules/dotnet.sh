621711d3b4583df5485d21da118388f994abc0fb
Robert Laverty <roblav96@gmail.com>
2020-06-13 03:13:32 -0400

    [Darwin] M  configs/starship.toml

--- modules/dotnet.sh
+++ modules/dotnet.sh
@@ -1,7 +1,7 @@
 export DOTNET_CLI_TELEMETRY_OPTOUT="1"
 
-_dotnet() {
-	local completions=("$(dotnet complete "$words")")
-	reply=( "${(ps:\n:)completions}" )
-}
-compctl -K _dotnet dotnet
+# _dotnet() {
+# 	local completions=("$(dotnet complete "$words")")
+# 	reply=( "${(ps:\n:)completions}" )
+# }
+# compctl -K _dotnet dotnet
