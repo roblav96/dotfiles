d098bbc1d14709cebaef2f9a42606f3fb50c76bb
Robert Laverty <roblav96@gmail.com>
2020-06-22 22:47:26 -0400

    [Darwin] M  modules/exa.sh

--- modules/exa.sh
+++ modules/exa.sh
@@ -4,12 +4,12 @@ export EXA_COLORS="uu=2;37:gu=2;3;37:da=32:un=31:gn=2;3;31:lc=2;3;37:lm=3;37"
 local EXA_FLAGS="--long --all --header --classify --group --color-scale --color=always"
 # realpath "$(which -p exa)" | grep -q 'HEAD' && EXA_FLAGS="$EXA_FLAGS --icons"
 
-alias la="exa $EXA_FLAGS --extended --links --tree --level=1"
-alias lar="exa $EXA_FLAGS --extended --links --tree --level=2"
-alias larr="exa $EXA_FLAGS --extended --links --tree --level=3"
-alias larrr="exa $EXA_FLAGS --extended --links --tree --level=4"
-alias larrrr="exa $EXA_FLAGS --extended --links --tree --level=5"
-alias lara="exa $EXA_FLAGS --extended --links --tree"
+alias la="exa $EXA_FLAGS --extended --links --tree --level=1 --ignore-glob='.git'"
+alias lar="exa $EXA_FLAGS --extended --links --tree --level=2 --ignore-glob='.git'"
+alias larr="exa $EXA_FLAGS --extended --links --tree --level=3 --ignore-glob='.git'"
+alias larrr="exa $EXA_FLAGS --extended --links --tree --level=4 --ignore-glob='.git'"
+alias larrrr="exa $EXA_FLAGS --extended --links --tree --level=5 --ignore-glob='.git'"
+alias lara="exa $EXA_FLAGS --extended --links --tree --ignore-glob='.git'"
 
 local EXA_FLAGS="$EXA_FLAGS --ignore-glob='.git|.DS_Store'"
 
