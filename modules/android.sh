376e2a789df39f2855a3f1235b18630c84be9794
Robert Laverty <roblav96@gmail.com>
2020-06-26 02:13:50 -0400

    [Darwin] M  configs/.prettierrc

--- modules/android.sh
+++ modules/android.sh
@@ -11,10 +11,11 @@ export ANDROID_SDK_ROOT="$ANDROID_HOME"
 alias gradlew="\$PWD/gradlew"
 # alias gradle="gradle --no-daemon"
 
-function apksign() {
-	rm -f "$*-signed.apk"
-	rm -f "$*-unsigned-aligned.apk"
-	zipalign -v -p 4 "$*-unsigned.apk" "$*-unsigned-aligned.apk"
-	apksigner sign --ks "$HOME/.android/release.keystore" --out "$*-signed.apk" "$*-unsigned-aligned.apk"
-	apksigner verify "$*-signed.apk"
-}
+alias apksign='java -jar $ANDROID_HOME/uber-apk-signer-1.1.0.jar --verbose --overwrite --ks ~/.android/release.keystore --ksAlias androidreleasekey --apks'
+# function apksign() {
+# 	rm -f "$*-signed.apk"
+# 	rm -f "$*-unsigned-aligned.apk"
+# 	zipalign -v -p 4 "$*-unsigned.apk" "$*-unsigned-aligned.apk"
+# 	apksigner sign --ks "$HOME/.android/release.keystore" --out "$*-signed.apk" "$*-unsigned-aligned.apk"
+# 	apksigner verify "$*-signed.apk"
+# }
