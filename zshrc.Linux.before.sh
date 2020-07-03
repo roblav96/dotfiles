6ea93828adbbba9b6ceb8f1e95a9089cd52e2602
Robert Laverty <roblav96@gmail.com>
2020-07-02 21:21:53 -0400

    [Darwin] M  zshrc.Linux.before.sh

--- zshrc.Linux.before.sh
+++ zshrc.Linux.before.sh
@@ -19,7 +19,10 @@ export VISUAL="nano"
 # fi
 
 # [[ -d "/home/linuxbrew/.linuxbrew/share/zsh/functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/functions:$FPATH"
-[[ -d "/home/linuxbrew/.linuxbrew/share/zsh/site-functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/site-functions:$FPATH"
+# [[ -d "/home/linuxbrew/.linuxbrew/share/zsh/site-functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/site-functions:$FPATH"
+if [[ -d "/home/linuxbrew/.linuxbrew/share/zsh/site-functions" && ":$FPATH:" != *":/home/linuxbrew/.linuxbrew/share/zsh/site-functions:"* ]]; then
+	export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/site-functions:$FPATH"
+fi
 
 # export FZF_BASE="$HOME/.fzf"
 # [ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh
