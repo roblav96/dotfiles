0b80ce7bbd5f15f1f7cc10412fd40b476fcd4190
Robert Laverty <roblav96@gmail.com>
2020-07-02 20:54:44 -0400

    [Darwin] M  zshrc.Linux.before.sh

--- zshrc.Linux.before.sh
+++ zshrc.Linux.before.sh
@@ -18,8 +18,8 @@ export VISUAL="nano"
 # 	export OPENSSL_ROOT_DIR="/home/linuxbrew/.linuxbrew/opt/openssl"
 # fi
 
-# [[ -d "/home/linuxbrew/.linuxbrew/share/zsh/functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/functions:$FPATH"
-# [[ -d "/home/linuxbrew/.linuxbrew/share/zsh/site-functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/site-functions:$FPATH"
+[[ -d "/home/linuxbrew/.linuxbrew/share/zsh/functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/functions:$FPATH"
+[[ -d "/home/linuxbrew/.linuxbrew/share/zsh/site-functions" ]] && export FPATH="/home/linuxbrew/.linuxbrew/share/zsh/site-functions:$FPATH"
 
 # export FZF_BASE="$HOME/.fzf"
 # [ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh
