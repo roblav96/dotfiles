ecbb1fdb683b3038868038db853fc8d1c5d4f6b6
Robert Laverty <roblav96@gmail.com>
2020-07-20 02:24:23 -0400

    [Darwin] M  modules/android.sh

--- modules/android.sh
+++ modules/android.sh
@@ -1,6 +1,6 @@
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export ANDROID_SDK_ROOT="$ANDROID_HOME"
-# export ANDROID_NDK_HOME="$ANDROID_HOME/ndk-bundle"
+export ANDROID_NDK_HOME="$ANDROID_HOME/ndk-bundle"
 
 # export PATH="$PATH:$ANDROID_HOME/build-tools/29.0.3"
 # export PATH="$PATH:$ANDROID_HOME/ndk-bundle"
@@ -8,7 +8,7 @@ export ANDROID_SDK_ROOT="$ANDROID_HOME"
 # export PATH="$PATH:$ANDROID_HOME/tools/bin"
 # export PATH="$PATH:$ANDROID_HOME/tools"
 
-alias gradlew="\$PWD/gradlew"
+alias gradlew='$PWD/gradlew'
 # alias gradle="gradle --no-daemon"
 
 alias apksign='java -jar $ANDROID_HOME/uber-apk-signer-1.1.0.jar --verbose --ks ~/.android/release.keystore --ksAlias androidreleasekey --apks'
