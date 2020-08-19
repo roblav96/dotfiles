afcbf6ef6bd6179a609d71ff85837e660291361e
Robert Laverty <roblav96@gmail.com>
2020-08-19 00:33:14 -0400

    [Darwin] M  modules/git.sh

--- modules/git.sh
+++ modules/git.sh
@@ -22,13 +22,13 @@ alias gfo="git fetch origin"
 alias gpl="git pull"
 alias gpr="git pull --rebase"
 
-alias gi="git check-ignore --verbose **/{.,}* | sortt"
-alias gia="git check-ignore --verbose **/{.,}* --non-matching | sortt"
+alias gi="git check-ignore --verbose **/{.,}* | sortt | lscolors"
+alias gia="git check-ignore --verbose **/{.,}* --non-matching | sortt | lscolors"
 
 alias gclean="git clean -f -d -x"
 alias greset='git reset --hard origin/$(echo -n $(git rev-parse --abbrev-ref HEAD))'
-alias gcld="gclean --dry-run | sed 's#^Would remove ##' | lscolors"
-alias gcl="gcld; echo; read -q '?Would remove...?' && return 1; echo; gclean; greset"
+alias gcld='gclean --dry-run | sed "s#^Would remove ##" | lscolors'
+alias gcl='gcld; echo; read -q "?Would remove...?" && return 1; echo; gclean; greset'
 
 alias gtag='git fetch --tags && git checkout $(git describe --tags $(git rev-list --tags --max-count=1))'
 alias gpush='test ! -d .git && echo "fatal: not a git repository" && return 1 || echo && gss && echo && git add -A && git commit -a -m "[$(uname -o)] $(git status --null)" && git push origin $(echo -n $(git rev-parse --abbrev-ref HEAD))'
