9b096e148a5383c0e1a6d9bb428e02bf449034fa
Robert Laverty <roblav96@gmail.com>
2020-10-01 23:11:30 -0400

    [Darwin] M  termux/boot/termux-services.sh

--- termux/boot/termux-services.sh
+++ termux/boot/termux-services.sh
@@ -1,12 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+echo "🌕 termux-services.sh"
+
 if [ -x "$(which pgrep)" ]; then
 	[ -x "$(which termux-wake-lock)" ] && termux-wake-lock
+	echo "🌕 (pgrep -xo sshd) -> '$(pgrep -xo sshd)'"
 	[ -x "$(which sshd)" ] && pgrep -xo sshd >/dev/null || sshd
 	if [ -x "$(which tinyproxy)" ]; then
 		if [ -x "$(which sed)" ]; then
 			sed --in-place='' 's/^Allow/#Allow/' "$PREFIX/etc/tinyproxy/tinyproxy.conf"
 		fi
+		echo "🌕 (pgrep -xo tinyproxy) -> '$(pgrep -xo tinyproxy)'"
 		pgrep -xo tinyproxy >/dev/null || tinyproxy -c "$PREFIX/etc/tinyproxy/tinyproxy.conf"
 	fi
 fi
