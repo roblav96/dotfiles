59386ef4b656efa77c43ba41a2a15ee8f1aea322
Robert Laverty <roblav96@gmail.com>
2020-10-12 02:34:04 -0400

    [Darwin] M  modules/rustup.sh

--- modules/rustup.sh
+++ modules/rustup.sh
@@ -1,7 +1,7 @@
 alias ru="rustup --verbose"
 alias rucd="cd $HOME/.rustup"
 alias ruout="rustup check"
-alias ruupg="rustup update stable"
+alias ruupg='rustup update stable && rustup completions zsh rustup > $GENCOMPL_FPATH/_rustup && rustup completions zsh cargo > $GENCOMPL_FPATH/_cargo && zcomp'
 alias rucls="rustup component list --installed"
 alias rucin="rustup component add"
 
