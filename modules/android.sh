4ae06de46b896043e20036785897a7a4ce41eaed
Robert Laverty <roblav96@gmail.com>
2020-10-30 12:03:40 -0400

    [Darwin] M  modules/android.sh

--- modules/android.sh
+++ modules/android.sh
@@ -30,10 +30,7 @@ alias apkanalyzer="apkanalyzer --human-readable"
 alias apki="apkanalyzer apk summary"
 
 alias avdls="avdmanager list avd"
-local emu_opts="accel on -gpu host"
-alias emu="emulator $emu_opts -avd"
-alias emud="daemonize \$(which -p emulator) $emu_opts -avd"
-unset emu_opts
+alias emulatord="daemonize \$(which -p emulator) accel on -gpu host -avd"
 
 # alias smali="java -jar $ANDROID_HOME/smali-2.4.0.jar"
 # alias baksmali="java -jar $ANDROID_HOME/baksmali-2.4.0.jar"
