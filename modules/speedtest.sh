a0c954b0ad4d1447ca977faab793eac0408d1030
Robert Laverty <roblav96@gmail.com>
2020-11-03 14:23:36 -0500

    [Darwin] M  modules/speedtest.sh

--- modules/speedtest.sh
+++ modules/speedtest.sh
@@ -4,3 +4,11 @@ alias linode="wget --output-document=/dev/null http://speedtest.newark.linode.co
 alias ievbox="wget --output-document=/dev/null https://az792536.vo.msecnd.net/vms/VMBuild_20190311/VirtualBox/MSEdge/MSEdge.Win10.VirtualBox.zip"
 
 alias iperf="iperf3 -c 192.34.85.234 -p 15201 --verbose"
+
+# speedtest "https://rose.listperfect.xyz/100Mio.dat?no-cache=$(date +%s)000"
+# speedtest "https://sweetfairlady-sto.energycdn.com/100Mio.dat?no-cache=$(date +%s)000"
+# speedtest "https://sweetfairlady.b-cdn.net/100Mio.dat?no-cache=$(date +%s)000"
+# speedtest "https://sweetfairlady-nyc1.energycdn.com/100Mio.dat?no-cache=$(date +%s)000"
+# speedtest "https://sweetfairlady-los1.energycdn.com/100Mio.dat?no-cache=$(date +%s)000"
+# speedtest "https://sweetfairlady-mia1.energycdn.com/100Mio.dat?no-cache=$(date +%s)000"
+# speedtest "https://cdn2-ovh-mon1.energycdn.com/100Mio.dat?no-cache=$(date +%s)000"
