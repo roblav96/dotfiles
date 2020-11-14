f2bcc10b6efff87593c51d744b4bd3546d6ada0e
Robert Laverty <roblav96@gmail.com>
2020-11-14 13:08:49 -0500

    [Darwin] M  modules/tldr.sh

--- modules/tldr.sh
+++ modules/tldr.sh
@@ -8,11 +8,11 @@ if [[ -x "$(which -p tldr)" ]]; then
 	function tlsr() {
 		local cache_dir_pages="$TEALDEER_CACHE_DIR/tldr-master/pages"
 		local cache_dir_os="$([[ "$PLATFORM" == "Darwin" ]] && echo "osx" || echo "linux")"
-		local files=($(rg --files-with-matches --smart-case --fixed-strings --word-regexp "$*" "$cache_dir_pages/common" "$cache_dir_pages/$cache_dir_os" | sort))
+		local files=($(rg --files-with-matches --smart-case --regexp "$*" "$cache_dir_pages/common" "$cache_dir_pages/$cache_dir_os" | sort))
 		local file && for file in "${files[@]}"; do
-			local name="$(basename "$file" '.md')"
+			local name="$(basename "$file" ".md")"
 			echo "🌕 '$name'"
-			tldr --color=always "$name" | rg --passthru --smart-case --fixed-strings --word-regexp "$*"
+			tldr --color=always "$name" | rg --passthru --smart-case --regexp "$*"
 		done
 		# local lines=( ${"$(rg --files-with-matches --smart-case --fixed-strings --word-regexp "$*" "$cache_dir_pages/common" "$cache_dir_pages/$cache_dir_os")"} )
 		# rg --files-with-matches --smart-case --fixed-strings --word-regexp "$*" "$cache_dir_pages/common" "$cache_dir_pages/$cache_dir_os" | rargs -p '.*/(.*).md' tldr {1} | rg --passthru --smart-case --fixed-strings --word-regexp "$*"
