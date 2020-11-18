93fba795f5ca3026e52786fb898de637de4ed491
Robert Laverty <roblav96@gmail.com>
2020-11-18 12:19:53 -0500

    [Darwin] M  modules/diff.sh

--- modules/diff.sh
+++ modules/diff.sh
@@ -1,4 +1,4 @@
 alias diffs="icdiff --highlight --line-numbers --unified=3"
-alias diffc='diff <($(fc -ln -1 -1)) <($(fc -ln -2 -2)) | diffr'
+alias diffc='diff -u <($(fc -ln -2 -2)) <($(fc -ln -1 -1)) | delta'
 
 alias checksum="sha256sum"
