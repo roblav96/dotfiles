accef4f0b60b8955449857dfdda0918cd73be5ae
Robert Laverty <roblav96@gmail.com>
2021-01-25 04:52:09 -0500

    [Darwin] M  modules/git.sh

--- modules/git.sh
+++ modules/git.sh
@@ -1,4 +1,4 @@
-export GIT_PAGER="$PAGER"
+# export GIT_PAGER="$PAGER"
 [[ -x "$(which -p gh)" ]] && export GH_NO_UPDATE_NOTIFIER="1"
 [[ -x "$(which -p gh)" ]] && export GH_PAGER=""
 [[ -x "$(which -p git-restore-mtime)" ]] && alias {gmt,grmt}="git-restore-mtime --force"
