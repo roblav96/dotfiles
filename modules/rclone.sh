50eb092c0b574b6061355468bb3952b6e2353541
Robert Laverty <roblav96@gmail.com>
2021-02-15 10:30:40 -0500

    [Darwin] M  modules/rclone.sh

--- modules/rclone.sh
+++ modules/rclone.sh
@@ -6,7 +6,7 @@ function rcwd() {
 		local dir="$(realpath "$v")"
 		local port="$(porthash "$dir")"
 		local base="$(basename "$dir")"
-		echo rclone serve webdav "$dir" --addr "0.0.0.0:$port" --baseurl "$base" --read-only --config /dev/null
+		echo rclone serve webdav "'$dir'" --addr "$(getip):$port" --baseurl "'$base'" --read-only
 		# echo && echo "🟢 http://$(getip):$port/$base/ -> $dir"
 		# if which pueue &>/dev/null; then
 		# 	pueue add --group=webdav -- rclone serve webdav "$dir" --addr "0.0.0.0:$port" --baseurl "$base" --read-only --config /dev/null
