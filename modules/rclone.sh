8ca70253b1c7b20f5a897831929fbffd5b7ffbac
Robert Laverty <roblav96@gmail.com>
2021-02-15 09:22:46 -0500

    [Darwin] M  modules/rclone.sh

--- modules/rclone.sh
+++ modules/rclone.sh
@@ -3,13 +3,15 @@ alias rc="rclone"
 function rcwd() {
 	local v && for v in "$@"; do
 		[[ ! -d "$v" ]] && continue
-		local port="$(porthash "$v")"
-		local baseurl="$(basename "$v")"
-		echo && echo "🟢 http://$(getip):$port/$baseurl/ -> $v"
-		if which pueue &>/dev/null; then
-			pueue add --group=webdav -- rclone serve webdav "$v" --addr "0.0.0.0:$port" --baseurl "$baseurl" --read-only --config /dev/null
-		else
-			echo rclone serve webdav "$v" --addr "0.0.0.0:$port" --baseurl "$baseurl" --read-only --config /dev/null --verbose
-		fi
+		local dir="$(realpath "$v")"
+		local port="$(porthash "$dir")"
+		local base="$(basename "$dir")"
+		echo rclone serve webdav "$dir" --addr "0.0.0.0:$port" --baseurl "$base" --read-only --config /dev/null
+		# echo && echo "🟢 http://$(getip):$port/$base/ -> $dir"
+		# if which pueue &>/dev/null; then
+		# 	pueue add --group=webdav -- rclone serve webdav "$dir" --addr "0.0.0.0:$port" --baseurl "$base" --read-only --config /dev/null
+		# else
+		# 	echo rclone serve webdav "$dir" --addr "0.0.0.0:$port" --baseurl "$base" --read-only --config /dev/null --verbose
+		# fi
 	done
 }
