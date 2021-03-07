d316d4361a46b623df550bb1be943898b4857cd8
Robert Laverty <roblav96@gmail.com>
2021-03-07 15:45:12 -0500

    [Darwin] A  configs/youtube-dl/soundcloud.com.conf

--- modules/youtubedl.sh
+++ modules/youtubedl.sh
@@ -3,6 +3,8 @@ alias yt="youtube-dl"
 alias ytdl="youtube-dl --restrict-filenames"
 alias ytls="youtube-dl --restrict-filenames --list-formats"
 
+alias ytsc='youtube-dl --restrict-filenames --config-location $DOTFILES/configs/youtube-dl/soundcloud.com.conf --add-header "$SOUNDCLOUD_OAUTH"'
+
 function ytmpv() {
 	youtube-dl --output - "$*" | mpv --force-seekable=yes -
 }
