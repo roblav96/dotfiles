5f089643dc0a869a98db3f8b4a5391d548cd7412
Robert Laverty <roblav96@gmail.com>
2021-04-02 05:27:04 -0400

    [Darwin] M  zshrc.Darwin.before.sh

--- zshrc.Darwin.before.sh
+++ zshrc.Darwin.before.sh
@@ -6,7 +6,7 @@
 # export PASTEL_COLOR_MODE="8bit"
 # export SDKROOT="macosx10.14"
 # export SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk"
-export MACOSX_DEPLOYMENT_TARGET="10.15"
+# export MACOSX_DEPLOYMENT_TARGET="10.15"
 
 # export JAVA_HOME="$(/usr/libexec/java_home)"
 # export PATH="$HOME/.jenv/bin:$PATH"
