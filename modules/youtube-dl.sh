73215b2a59142e3d682c909d970828f7796cc415
Robert Laverty <roblav96@gmail.com>
2021-04-06 00:58:26 -0400

    [Darwin] A  configs/youtube-dl.conf

--- modules/youtube-dl.sh
+++ modules/youtube-dl.sh
@@ -1,15 +1,14 @@
-alias youtube-dl="youtube-dl --no-call-home"
 alias yt="youtube-dl"
-alias ytdl="youtube-dl --restrict-filenames"
-alias ytls="youtube-dl --restrict-filenames --list-formats"
+alias ytls="youtube-dl --list-formats"
 
-function ytsc() {
-	youtube-dl --restrict-filenames --ignore-errors \
-		--output '%(uploader)s/%(uploader)s__%(title)s.%(ext)s' --add-header "$SOUNDCLOUD_OAUTH" \
-		--extract-audio --audio-format=m4a --add-metadata --embed-thumbnail \
-		"$@"
-} && compdef ytsc=youtube-dl
-# alias ytsc="$(echo 'youtube-dl --restrict-filenames --ignore-errors
+alias sc='youtube-dl --output="%(uploader)s/%(uploader)s__%(title)s.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH" --extract-audio --audio-format=m4a --add-metadata --embed-thumbnail'
+# function sc() {
+# 	youtube-dl --ignore-errors \
+# 		--output '%(uploader)s/%(uploader)s__%(title)s.%(ext)s' --add-header "$SOUNDCLOUD_OAUTH" \
+# 		--extract-audio --audio-format=m4a --add-metadata --embed-thumbnail \
+# 		"$@"
+# } && compdef ytsc=youtube-dl
+# alias sc="$(echo 'youtube-dl --restrict-filenames --ignore-errors
 #  --output="%(uploader)s/%(uploader)s__%(title)s.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH"
 #  --extract-audio --audio-format=m4a --add-metadata --embed-thumbnail
 # ' | tr -d '\n')"
