c7795512db04980de2a097fa87a52d843cbd0407
Robert Laverty <roblav96@gmail.com>
2021-04-17 01:42:05 -0400

    [Darwin] M  modules/rclone.sh

--- modules/rclone.sh
+++ modules/rclone.sh
@@ -4,7 +4,7 @@ function rcwd() {
 	local v && for v in "$@"; do
 		[[ ! -d "$v" ]] && continue
 		local dir="$(realpath "$v")"
-		echo rclone serve webdav "$dir" --addr "$(getip):$(porthash "$dir")" --baseurl "$(basename "$dir")" --read-only
+		echo rclone serve webdav "'$dir'" --addr "$(getip):$(porthash "$dir")" --baseurl "'$(basename "$dir")'" --read-only
 		# --local-no-check-updated --local-no-set-modtime
 	done
 }
