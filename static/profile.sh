5d236a2a95ddd98ed7893da6dfe6e88786378903
Robert Laverty <roblav96@gmail.com>
2021-04-19 09:27:07 -0400

    [Darwin] M  static/profile.sh

--- static/profile.sh
+++ static/profile.sh
@@ -44,7 +44,7 @@ fi
 export GREP_COLOR="01;31"
 export GREP_COLORS="ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36"
 alias grep="grep --color=auto" # --ignore-case --extended-regexp"
-alias g="grep --fixed-strings"
+alias g="grep --fixed-strings --ignore-case"
 
 # unalias l
 # unalias ll
