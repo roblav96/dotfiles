c3cf9393f1aae046beb57233f5c83449d52c7628
Robert Laverty <roblav96@gmail.com>
2021-04-24 07:03:44 -0400

    [Darwin] D  modules/daemonize.sh

--- modules/daemonize.sh
+++
@@ -1,2 +0,0 @@
-
-alias init.daemonize="daemonize -u nobody -c $HOME -- /usr/bin/env -i HOME=$HOME $(which bash) -l -c"
--- static/profile.sh
+++ static/profile.sh
@@ -214,9 +214,10 @@ test -x "$(which wget)" && source "$DOTFILES/modules/speedtest.sh"
 
 # [[ -d "/mnt/WD_GRAPHITE/idk_SMBv2" ]] && alias cdidk="cd /mnt/WD_GRAPHITE/idk_SMBv2"
 
-test -x "$(which daemonize)" && source "$DOTFILES/modules/daemonize.sh"
 test -x "$(which ffmpeg)" && source "$DOTFILES/modules/ffmpeg.sh"
 test -x "$(which rclone)" && source "$DOTFILES/modules/rclone.sh"
+
+test -x "$(which daemonize)" && alias init.daemonize="daemonize -u nobody -c $HOME -- /usr/bin/env -i HOME=$HOME $(which bash) -l -c"
 
 if [[ -n "$PS1" && -f "/opt/etc/profile.d/bash_completion.sh" ]]; then
 	. "/opt/etc/profile.d/bash_completion.sh"
