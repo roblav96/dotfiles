c441186a726759c677eaf5c76bc5b96fc111a474
Robert Laverty <roblav96@gmail.com>
2021-06-19 19:56:40 -0400

    [Darwin] M  modules/youtube-dl.sh

--- modules/youtube-dl.sh
+++ modules/youtube-dl.sh
@@ -1,11 +1,13 @@
 alias yt="youtube-dl"
 alias ytls="youtube-dl --list-formats"
 
-alias ytsc="youtube-dl \
---output=\"%(uploader)s/%(uploader)s__%(title)s.%(ext)s\" \
---add-header=\"\$SOUNDCLOUD_OAUTH\" \
---extract-audio --audio-format=m4a \
---add-metadata --embed-thumbnail"
+alias ytsc='youtube-dl --output="%(uploader)s/%(uploader)s__%(title)s__%(view_count)s_%(like_count)s_.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH" --extract-audio --add-metadata --embed-thumbnail'
+
+# alias ytsc="youtube-dl \
+# --output='%(uploader)s/%(uploader)s__%(title)s.%(ext)s' \
+# --add-header=\"\$SOUNDCLOUD_OAUTH\" \
+# --extract-audio --add-metadata --embed-thumbnail"
+# # --audio-format=m4a \
 
 # function sc() {
 # 	youtube-dl --ignore-errors \
