d610da1989b3a91d4123d827a70932321da61c0c
Robert Laverty <roblav96@gmail.com>
2021-07-03 10:30:20 -0400

    [Darwin] M  configs/.prettierrc

--- modules/adb.sh
+++ modules/adb.sh
@@ -34,8 +34,8 @@ which cltv &>/dev/null || alias cltv="curltv"
 # ████  install adb busybox  ████
 # adb push busybox-arm64 /data/local/tmp/bin/busybox; adb shell /data/local/tmp/bin/busybox --install -s /data/local/tmp/bin
 alias adbshell="echo; echo 'export PATH=/data/local/tmp/bin:\$PATH'; echo; adb shell"
-alias adbfd="adb shell /data/local/tmp/bin/fd -uu --color=always --base-directory=/ --exclude=/dev --exclude=/proc --exclude=/sys --absolute-path --fixed-strings"
-alias adbl="adb shell /data/local/tmp/bin/ls --color=always -laph"
+alias adbfd="adb exec-out /data/local/tmp/bin/fd -uu --base-directory=/ --exclude=/dev --exclude=/proc --exclude=/sys --absolute-path --fixed-strings"
+alias adbl="adb exec-out /data/local/tmp/bin/ls --color=auto -laph"
 
 # alias rogcat='rogcat $([[ $(tput cols) -lt 125 ]] && echo --hide-timestamp)'
 # alias rogcat='rogcat $([[ $(tput cols) -lt 125 ]] && echo --hide-timestamp) --buffer all --level trace --message "!^loading \[eventTime=\d" --tag "!^netstats_\w+_sample$"'
