9b4c48da4a7c69c0e1b2f9645ccebebae5bab997
Robert Laverty <roblav96@gmail.com>
2021-08-17 04:41:04 -0400

    [Darwin] M  modules/rustup.sh

--- modules/rustup.sh
+++ modules/rustup.sh
@@ -1,13 +1,14 @@
+if [[ -d "$HOME/.rustup" ]]; then
+    [[ -z "$RUSTUP_HOME" ]] && export RUSTUP_HOME="$HOME/.rustup"
+fi
+
 # alias ru="rustup"
-alias rucd="cd $HOME/.rustup"
+alias rucd="cd $RUSTUP_HOME/.rustup"
 
 alias ruout="rustup check"
 alias ruupg='rustup update stable && rustup completions zsh rustup > $GENCOMPL_FPATH/_rustup && rustup completions zsh cargo > $GENCOMPL_FPATH/_cargo && zcomp'
 
 alias rucls="rustup component list --installed"
-alias rucin="rustup component add"
-
 alias rutls="rustup target list --installed"
-alias rutin="rustup target install"
 
 alias rustfmt="rustfmt --config hard_tabs=true"
