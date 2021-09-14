9f11a9f6c55e79be9f93023724743b8774ce0ba2
Robert Laverty <roblav96@gmail.com>
2021-09-13 22:48:39 -0400

    [Darwin] M  modules/pkg-config.sh

--- modules/pkg-config.sh
+++ modules/pkg-config.sh
@@ -1,9 +1,11 @@
 #
 
 function pc_path() {
+	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}/usr/local/share/quictls/lib/pkgconfig"
 	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/openssl@1.1/lib/pkgconfig"
+	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/libressl/lib/pkgconfig"
 	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/python@3.9/lib/pkgconfig"
-	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/util-linux/lib/pkgconfig"
+	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/ossp-uuid/lib/pkgconfig"
 	rg --no-messages --files-with-matches --hidden --type=ruby keg_only "$(brew --prefix)/Cellar" | sort | while read i; do
 		local name="$(basename "$i" ".rb")"
 		local dir="$(brew --prefix)/opt/$name/lib/pkgconfig"
