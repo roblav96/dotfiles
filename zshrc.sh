88bbf05005b8a758c3b37c64631d493ec6d569a7
Robert Laverty <roblav96@gmail.com>
2021-09-16 22:03:47 -0400

    [Darwin] M  zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -218,8 +218,6 @@ alias ls="ls --color=auto"
 alias ll="ls -laph"
 alias diff="diff -u -x '.git'"
 alias tree="tree -N -a -I '.git'"
-alias esh='/usr/bin/env -i HOME=$HOME TERM=$TERM USER=$USER SHELL=/bin/sh /bin/sh'
-alias ebash='/usr/bin/env -i HOME=$HOME TERM=$TERM USER=$USER SHELL=/usr/local/bin/bash /usr/local/bin/bash'
 alias pwda="pwd && pwd -P"
 alias pathls="echo \$PATH | sed 's#:/#\n/#g'"
 # alias pathls="echo \$PATH | sed -e 's#:/#\n/#g' -e 's#:~#\n~#g'"
@@ -468,6 +466,9 @@ function pe() {
 # function p() {
 # 	ps auxww | grep -v grep | grep "$@"
 # }
+
+alias esh='/usr/bin/env -i HOME=$HOME TERM=$TERM USER=$USER SHELL=/bin/sh /bin/sh'
+alias ebash='/usr/bin/env -i HOME=$HOME TERM=$TERM USER=$USER SHELL=/usr/local/bin/bash /usr/local/bin/bash'
 
 function init.daemonize() {
 	# killall "$1" 2>/dev/null && sleep 1
