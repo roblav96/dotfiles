b5f1dab8822b5b3343bfbd528e8ca0af23f39bfd
Robert Laverty <roblav96@gmail.com>
2021-10-06 11:55:05 -0400

    [Darwin] M  modules/youtube-dl.rc

--- modules/youtube-dl.sh
+++ modules/youtube-dl.sh
@@ -3,20 +3,9 @@ alias yt-dlp="yt-dlp --no-call-home"
 
 alias yt="yt-dlp"
 alias ytls="yt-dlp --list-formats"
-alias ytsc='yt-dlp --output="%(uploader)s/%(uploader)s__%(title)s.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH" --extract-audio --add-metadata --embed-thumbnail'
-# --audio-format=m4a
 
-# function sc() {
-# 	youtube-dl --ignore-errors \
-# 		--output '%(uploader)s/%(uploader)s__%(title)s.%(ext)s' --add-header "$SOUNDCLOUD_OAUTH" \
-# 		--extract-audio --audio-format=m4a --add-metadata --embed-thumbnail \
-# 		"$@"
-# } && compdef ytsc=youtube-dl
-# alias sc="$(echo 'youtube-dl --restrict-filenames --ignore-errors
-#  --output="%(uploader)s/%(uploader)s__%(title)s.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH"
-#  --extract-audio --audio-format=m4a --add-metadata --embed-thumbnail
-# ' | tr -d '\n')"
+alias sc='yt-dlp --output="%(uploader)s/%(uploader)s__%(title)s.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH" --extract-audio'
 
 function ytmpv() {
-	youtube-dl --output - "$*" | mpv --force-seekable=yes -
+	yt --output - "$*" | mpv --msg-module --msg-level=all=status --force-seekable=yes -
 }
