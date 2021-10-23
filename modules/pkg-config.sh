b95372a5c939194014dff818b73ddb5bddd302ed
Robert Laverty <roblav96@gmail.com>
2021-10-23 13:58:31 -0400

    [Darwin] M  modules/pkg-config.sh

--- modules/pkg-config.sh
+++ modules/pkg-config.sh
@@ -11,17 +11,18 @@ function pkgfind() {
 } && compdef pkgfind=pkg-config
 
 function pc_path() {
-	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/openssl@1.1/lib/pkgconfig"
-	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/libressl/lib/pkgconfig"
-	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/python@3.9/lib/pkgconfig"
-	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/python@3.8/lib/pkgconfig"
-	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$(brew --prefix)/opt/ossp-uuid/lib/pkgconfig"
-	# rg --no-messages --files-with-matches --hidden --type=ruby keg_only "$(brew --prefix)/Cellar" | sortt | while read i; do
 	local prefix="$(brew --prefix)"
+	export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$prefix/opt/openssl@3/lib/pkgconfig:$prefix/opt/ruby@2.7/lib/pkgconfig"
+	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$prefix/opt/libressl/lib/pkgconfig"
+	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$prefix/opt/python@3.9/lib/pkgconfig"
+	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$prefix/opt/python@3.8/lib/pkgconfig"
+	# export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}$prefix/opt/ossp-uuid/lib/pkgconfig"
+	# rg --no-messages --files-with-matches --hidden --type=ruby keg_only "$prefix/Cellar" | sortt | while read i; do
 	rg --no-messages --files-with-matches --type=ruby --fixed-strings 'keg_only ' $prefix/Homebrew/Library/Taps/*/*/Formula | sortt | while read i; do
 		local name="$(basename "$i" ".rb")"
 		[[ "$name" == "libressl" ]] && continue
 		[[ "$name" == "openssl@1.1" ]] && continue
+		[[ "$name" == "openssl@3" ]] && continue
 		[[ "$name" != "${name/'@'/''}" ]] && continue
 		local dir="$prefix/opt/$name/lib/pkgconfig"
 		if [[ -d "$dir" && "$PKG_CONFIG_PATH" != *"$dir"* ]]; then
