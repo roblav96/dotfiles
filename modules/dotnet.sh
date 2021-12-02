720aab1b7f6276968aacfbc1eae2d77737509aa7
Robert Laverty <roblav96@gmail.com>
2021-12-02 07:54:32 -0500

    [Darwin] M  configs/.gemrc

--- modules/dotnet.sh
+++ modules/dotnet.sh
@@ -1,8 +1,10 @@
 [[ -z "$DOTNET_CLI_TELEMETRY_OPTOUT" ]] && export DOTNET_CLI_TELEMETRY_OPTOUT="1"
 
-which dn &>/dev/null || alias dn="dotnet"
-which mo &>/dev/null || alias mo="mono"
+# which dn &>/dev/null || alias dn="dotnet"
+# which mo &>/dev/null || alias mo="mono"
 
+alias cake="dotnet cake"
+alias msbuild="dotnet msbuild"
 alias nuget="dotnet nuget"
 
 # _dotnet() {
