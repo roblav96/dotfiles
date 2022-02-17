c2c21f4fdeab968a19041a9520601e27b9f65890
Robert Laverty <roblav96@gmail.com>
2022-02-17 17:28:53 -0500

    [Darwin] M  modules/bat.rc

--- modules/ripgrep.sh
+++ modules/ripgrep.sh
@@ -1,10 +1,9 @@
-declare rg_flags="--heading --line-number --no-messages --smart-case --hidden --fixed-strings --context=2 --context-separator='' \
---glob='!.git' --glob='!.DS_Store' \
---type-add='pkgconfig:*.pc' --type-add='xml:*.plist' --type-add='gradle:*.kts'"
+[[ -e "$HOME/.config/ripgrep/config" ]] && export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/config"
+
+declare rg_flags="--heading --line-number --no-messages --smart-case --hidden --fixed-strings --context=2 --context-separator=''"
 # --colors=match:bg:16 --colors=path:bg:16
 # --colors=path:fg:green --colors=path:style:bold
 declare rg_max_columns_flags="--max-columns-preview --max-columns=\$(expr \$(tput cols) - 10)"
-# export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
 
 alias r="rg $rg_flags $rg_max_columns_flags --glob='!node_modules'"
 alias rl="rg $rg_flags --glob='!node_modules'"
@@ -13,7 +12,7 @@ alias ral="rg $rg_flags --no-ignore --stats"
 alias raa="rg $rg_flags $rg_max_columns_flags --no-ignore --stats --follow -uuu"
 alias raal="rg $rg_flags --no-ignore --stats --follow -uuu"
 
-alias rfs="rg -uu --files-with-matches --no-messages --smart-case --glob='!.git' --fixed-strings"
+unset rg_flags rg_max_columns_flags
 
 unalias g &>/dev/null
 alias g="rg --smart-case --fixed-strings"
@@ -22,9 +21,16 @@ alias gp="rg --smart-case --fixed-strings --passthru"
 alias gw="rg --smart-case --fixed-strings --word-regexp"
 alias gx="rg --smart-case --regexp"
 
-alias rgls="rg --type-list"
+alias rfs="rg -uu --files-with-matches --no-messages --smart-case --glob='!.git' --fixed-strings"
 
-unset rg_flags rg_max_columns_flags
+alias rgls="rg --type-list | bl fish"
+# function rgls() {
+# 	if [[ $# -eq 0 ]]; then
+# 		rg --type-list | bat --style=grid -l fish
+# 	else
+# 		rg --type-list | g --color=always "$@" | bat --style=grid -l fish
+# 	fi
+# }
 
 #
 
