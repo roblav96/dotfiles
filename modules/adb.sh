eb619d5b6b1f2bb557ce34b437aa877416e23126
Robert Laverty <roblav96@gmail.com>
2022-02-27 11:23:13 -0500

    [Darwin] M  modules/adb.sh

--- modules/adb.sh
+++ modules/adb.sh
@@ -19,12 +19,14 @@ alias adbre='adb kill-server && sleep 1; adb connect $ANDROID_SERIAL && adb devi
 
 # alias clp="curl --proxy $ANDROID_SERIAL:11080"
 function cltv() {
+	local proxy="$ANDROID_SERIAL:11080" && curl --silent "$proxy" 2>/dev/null || proxy="$(getrouter):11080"
+	# local proxy="$(getrouter):11080" && curl --silent "$proxy" 2>/dev/null || proxy="$ANDROID_SERIAL:11080"
 	if [[ "$1" == "premiumize" ]]; then
-		curl --proxy "$ANDROID_SERIAL:11080" "https://www.premiumize.me/api/transfer/directdl?customer_id=$PREMIUMIZE_ID&pin=$PREMIUMIZE_PIN&src=magnet:?xt=urn:btih:$2" | jq '.content' | jq "map(select(.link|endswith(\"${3:-mkv}\")))" | jq 'map(.link)[]' --raw-output | sortt --field-separator='/' --key=10
+		curl --proxy "$proxy" "https://www.premiumize.me/api/transfer/directdl?customer_id=$PREMIUMIZE_ID&pin=$PREMIUMIZE_PIN&src=magnet:?xt=urn:btih:$2" | jq '.content' | jq "map(select(.link|endswith(\"${3:-mkv}\")))" | jq 'map(.link)[]' --raw-output | sortt --field-separator='/' --key=10
 	elif [[ "$1" == "real-debrid" ]]; then
-		curl --proxy "$ANDROID_SERIAL:11080" "https://api.real-debrid.com/rest/1.0/unrestrict/link?auth_token=$REALDEBRID_SECRET" -d "link=https://real-debrid.com/d/$2" | jq '.download' --raw-output
+		curl --proxy "$proxy" "https://api.real-debrid.com/rest/1.0/unrestrict/link?auth_token=$REALDEBRID_SECRET" -d "link=https://real-debrid.com/d/$2" | jq '.download' --raw-output
 	elif [[ "$1" == "alldebrid" ]]; then
-		curl --proxy "$ANDROID_SERIAL:11080" "https://api.alldebrid.com/v4/link/unlock?agent=$ALLDEBRID_AGENT&apikey=$ALLDEBRID_KEY&link=https://uptobox.com/$2" | jq '.data.link' --raw-output
+		curl --proxy "$proxy" "https://api.alldebrid.com/v4/link/unlock?agent=$ALLDEBRID_AGENT&apikey=$ALLDEBRID_KEY&link=https://uptobox.com/$2" | jq '.data.link' --raw-output
 	fi
 }
 
@@ -415,9 +417,9 @@ function adbrclone() {
 	adb shell /data/local/tmp/bin/start-stop-daemon -S -b -p /dev/null \
 		-x /data/local/tmp/bin/rclone -- --config /data/local/tmp/rclone.conf \
 		serve dlna "premiumize:" --name "premiumize" --addr "$ip:$(porthash "premiumize")" --read-only --no-modtime
-	# adb shell /data/local/tmp/bin/start-stop-daemon -S -b -p /dev/null \
-	# 	-x /data/local/tmp/bin/rclone -- --config /data/local/tmp/rclone.conf \
-	# 	serve dlna "megadav:" --name "megadav" --addr "$ip:$(porthash "megadav")" --read-only --no-modtime
+	adb shell /data/local/tmp/bin/start-stop-daemon -S -b -p /dev/null \
+		-x /data/local/tmp/bin/rclone -- --config /data/local/tmp/rclone.conf \
+		serve dlna "megadav:" --name "megadav" --addr "$ip:$(porthash "megadav")" --read-only --no-modtime
 	adb shell /data/local/tmp/bin/start-stop-daemon -S -b -p /dev/null \
 		-x /data/local/tmp/bin/rclone -- --config /data/local/tmp/rclone.conf \
 		serve dlna "WD_GRAPHITE:" --name "WD_GRAPHITE" --addr "$ip:$(porthash "WD_GRAPHITE")" --read-only --no-modtime
