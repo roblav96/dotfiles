3c6076bef12ed0d8a2247691f398b969885bb296
Robert Laverty <roblav96@gmail.com>
2022-03-04 14:05:16 -0500

    [Darwin] M  termux/boot/termux-gost.sh

--- termux/boot/termux-gost.sh
+++ termux/boot/termux-gost.sh
@@ -5,5 +5,5 @@ echo "█ termux-gost.sh"
 if [ -x $(command -v pgrep) ]; then
 	[ -x $(command -v termux-wake-lock) ] && termux-wake-lock
 	[ -x $(command -v termux-api-start) ] && termux-api-start
-	[ -x $(command -v gost) ] && pgrep -x gost >/dev/null || gost -L 'http://:11080?dns=1.1.1.1:53/tcp,1.1.1.1:853/tls,https://1.1.1.1/dns-query'
+	[ -x $(command -v gost) ] && pgrep -x gost >/dev/null || gost -L 'http://0.0.0.0:11080?dns=1.1.1.1:53/tcp,1.1.1.1:853/tls,https://1.1.1.1/dns-query'
 fi
