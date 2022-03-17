2585b9fa96c7ae3adf8a68367b4f03046d8e3dd0
Robert Laverty <roblav96@gmail.com>
2022-03-17 07:43:54 -0400

    [Darwin] M  configs/vlcrc

--- deno/stdin-downloader.ts
+++ deno/stdin-downloader.ts
@@ -1,15 +1,21 @@
 // import 'https://raw.githubusercontent.com/roblav96/jellyfin-debrids/main/src/devops/console.ts'
 
 import * as io from 'https://deno.land/std/io/mod.ts'
-import * as signal from 'https://deno.land/std/signal/mod.ts'
-import * as streams from 'https://deno.land/std/streams/mod.ts'
 import R from 'https://esm.sh/rambdax?dev'
 
 Deno.setRaw(Deno.stdin.rid, true)
 for await (let stdin of io.iterateReader(Deno.stdin)) {
-	if (stdin.at(0) == 3) Deno.exit(130)
-	let line = new TextDecoder().decode(stdin)
-	console.log('line ->', line)
+	if (stdin.length == 1 && stdin.at(0) == 3) {
+		Deno.setRaw(Deno.stdin.rid, false)
+		Deno.exit(130)
+	}
+	// console.log('stdin ->', stdin)
+	// Deno.stdout.writeSync(stdin)
+	let input = new TextDecoder().decode(stdin)
+	let lines = input.split('\r').filter(Boolean)
+	// let lines = input.split('\r').filter(Boolean)
+
+	console.log('lines ->', lines)
 }
 
 // // let p = Buffer.alloc(size || 16 * 1024)
