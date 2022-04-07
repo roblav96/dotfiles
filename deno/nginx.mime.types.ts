99c9867482573b70c70a137462320ed64af1217f
Robert Laverty <roblav96@gmail.com>
2022-04-07 17:55:57 -0400

    [Darwin] M  deno/nginx.mime.types.ts

--- deno/nginx.mime.types.ts
+++ deno/nginx.mime.types.ts
@@ -4,18 +4,19 @@ import * as collections from 'https://deno.land/std/collections/mod.ts'
 import * as path from 'https://deno.land/std/path/mod.ts'
 import sortKeys from 'https://esm.sh/sort-keys?dev'
 
-let prefix = new TextDecoder().decode(
-	await Deno.run({
-		cmd: ['brew', '--prefix'],
-		stdout: 'piped',
-	}).output(),
-)
-let globs = await Deno.readTextFile(path.join(prefix.trim(), 'share/mime/globs'))
-let lines = globs.split('\n').filter((v) => v.includes(':*.'))
-let groups = collections.groupBy(lines, (v) => v.split(':*.')[0])
-let mimes = sortKeys(collections.mapValues(groups, (v) => v!.map((v) => v.split(':*.')[1]).sort()))
-let each = Object.entries(mimes).reduce((target, [key, value], index) => {
-	return target.concat(`    ${key} ${value.join(' ')};`)
+let db = (await (
+	await fetch('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json')
+).json()) as {
+	[mediaType: string]: {
+		source: string
+		compressible: boolean
+		charset: string
+		extensions: string[]
+	}
+}
+let each = Object.entries(db).reduce((target, [key, value], index) => {
+	if (!Array.isArray(value.extensions)) return target
+	return target.concat(`    ${key} ${value.extensions.join(' ')};`)
 }, [] as string[])
 let output = `
 types {
@@ -23,4 +24,26 @@ ${each.join('\n')}
 }
 `
 console.log(output)
+let prefix = new TextDecoder().decode(
+	await Deno.run({
+		cmd: ['brew', '--prefix'],
+		stdout: 'piped',
+	}).output(),
+)
 await Deno.writeTextFile(path.join(prefix.trim(), 'etc/nginx/mime.types'), output)
+
+
+// let globs = await Deno.readTextFile(path.join(prefix.trim(), 'share/mime/globs'))
+// let lines = globs.split('\n').filter((v) => v.includes(':*.'))
+// let groups = collections.groupBy(lines, (v) => v.split(':*.')[0])
+// let mimes = sortKeys(collections.mapValues(groups, (v) => v!.map((v) => v.split(':*.')[1]).sort()))
+// let each = Object.entries(mimes).reduce((target, [key, value], index) => {
+// 	return target.concat(`    ${key} ${value.join(' ')};`)
+// }, [] as string[])
+// let output = `
+// types {
+// ${each.join('\n')}
+// }
+// `
+// console.log(output)
+// await Deno.writeTextFile(path.join(prefix.trim(), 'etc/nginx/mime.types'), output)
