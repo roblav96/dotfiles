9267e25118ca8d8a22002798ba26d26472ae9d23
Robert Laverty <roblav96@gmx.com>
2022-09-15 11:51:44 -0400

    [Darwin] M  modules/npm.sh

--- modules/npm.sh
+++ modules/npm.sh
@@ -6,7 +6,7 @@ alias .node-v8-flags="node --help --v8-options | sed -e 's/^  --/\n  --/' -e 's/
 
 # alias n="npm"
 alias npx="npx --yes"
-alias rr="npm run-script"
+alias rr="npm run"
 alias npmls="npm ls"
 alias npmlsa="npm ls --all"
 alias npmo="npm outdated"
