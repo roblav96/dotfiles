ae283d4e27f749941533152beaee6b34b35cfdb2
Robert Laverty <roblav96@gmx.com>
2022-10-06 00:54:08 -0400

    [Darwin] M  modules/adb.sh

--- modules/adb.sh
+++ modules/adb.sh
@@ -64,6 +64,8 @@ alias rogcat="rogcat --hide-timestamp --buffer all \
 --message '!^Failed to find provider info for com.nvidia.ibeta$' \
 --message '!^NvRmStreamFree: WARN: pStream is NULL$' \
 --message '!^tsec_version: 1$' \
+--message '!^SecureHdcp_GetCurrentLevel: cur: 0x04, max: 0x04$' \
+--message '! OEMCrypto_Generic_' \
 \
 --message '!^Failed to find provider info for com.google.android.gsf.gservices$' \
 --message '!^handleComboKeys isOnHomeScreen: false$' \
@@ -116,7 +118,6 @@ rogs="$rogs --message '!^\[\d{3},\d{2},\d{4}\]$'"
 rogs="$rogs --message '!^skip emit ir command, use IR_MODE to send ir command$'"
 rogs="$rogs --message '!^Called isFriday vendor:\d+ product:\d+$'"
 rogs="$rogs --message '!^stop_output_stream: \d+ underruns seen on pcm device hdmi-playback$'"
-rogs="$rogs --message '!^SecureHdcp_GetCurrentLevel: cur: 0x04, max: 0x04$'"
 
 # kodi
 rogs="$rogs --message '!^onPlaybackStateChanged\(\): '"
