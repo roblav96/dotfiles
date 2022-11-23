502abeb5db2da92506e01870d31fe2de3fee6f42
Robert Laverty <roblav96@gmx.com>
2022-11-23 10:39:51 -0500

    [Darwin] M  zshrc.sh

--- zshrc.sh
+++ zshrc.sh
@@ -354,7 +354,7 @@ test -x "$(which -p fzf)" && source "$DOTFILES/modules/fzf.sh"
 test -x "$(which -p sk)" && source "$DOTFILES/modules/sk.sh"
 
 alias cdp="cd ~/.playground"
-alias bak='[[ -d ~/.playground/"$(basename "$PWD").$(dateiso)" ]] && rd ~/.playground/"$(basename "$PWD").$(dateiso)"; mkdir ~/.playground/"$(basename "$PWD").$(dateiso)" && f --no-ignore -d1 -X cp -r -t ~/.playground/"$(basename "$PWD").$(dateiso)"'
+alias bak='[[ -d ~/.playground/"$(basename "$PWD")" ]] && rd ~/.playground/"$(basename "$PWD")"; mkdir ~/.playground/"$(basename "$PWD")" && f --no-ignore -d1 -X cp -r -t ~/.playground/"$(basename "$PWD")"'
 
 alias cmdls="printf '%s\n' \$commands | sortt --unique"
 function cmdf() {
