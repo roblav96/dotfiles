92de585c7a4f50f1893ec49dd72b33db5d880869
Robert Laverty <roblav96@gmx.com>
2024-08-01 12:44:37 -0400

    [Darwin] M  modules/npm.sh

--- modules/youtube-dl.sh
+++ modules/youtube-dl.sh
@@ -1,6 +1,7 @@
 alias youtube-dl="youtube-dl --no-call-home"
 
 alias yt="yt-dlp"
+alias ytp=" pueue add -- yt-dlp"
 alias ytls="yt --list-formats"
 
 alias sc='yt --output="%(uploader)s/%(uploader)s__%(title)s.%(ext)s" --add-header="$SOUNDCLOUD_OAUTH" --extract-audio'
