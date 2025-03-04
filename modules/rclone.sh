13866e65efa4fc3e833d20ca74b64b5c65bb1de8
Robert Laverty <roblav96@gmx.com>
2025-03-04 09:59:30 -0500

    [Darwin] M  modules/rclone.sh

--- modules/rclone.sh
+++ modules/rclone.sh
@@ -11,7 +11,7 @@ function rcwd() {
 		elif [[ ! "$remote" =~ ":" ]]; then
 			remote="$remote:"
 		fi
-		echo rclone serve webdav "'$remote'" --baseurl "'$base'" --addr "$ip:18888" --read-only
+		echo rclone serve webdav "'$remote'" --baseurl "'$base'" --addr "$ip:18888" --read-only --include "'*.mp3'" --include "'*.mp4'" --include "'*.m4a'"
 		# echo rclone serve webdav "'$remote'" --baseurl "'$base'" --addr "$ip:$(porthash "$remote")" --read-only
 	done
 }
